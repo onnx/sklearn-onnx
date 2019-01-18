a40bb33b0345e47f5957732cdd761b4507815e7c
xavier dupré <xavier.dupre@gmail.com>
2019-01-18 13:19:04 +0100

    Disable test based not working with too old version of onnx

--- tests/test_SklearnRandomForestConverters.py
+++ tests/test_SklearnRandomForestConverters.py
@@ -19,7 +19,8 @@ class TestSklearnTreeEnsembleModels(unittest.TestCase):
         model = RandomForestClassifier(n_estimators=3)
         dump_one_class_classification(model)
         dump_binary_classification(model)
-        dump_multiple_classification(model)
+        dump_multiple_classification(model,
+                                     allow_failure="StrictVersion(onnx.__version__) < StrictVersion('1.2')")
 
     def test_random_forest_regressor(self):
         model = RandomForestRegressor(n_estimators=3)
@@ -30,7 +31,9 @@ class TestSklearnTreeEnsembleModels(unittest.TestCase):
         model = ExtraTreesClassifier(n_estimators=3)
         dump_one_class_classification(model)
         dump_binary_classification(model)
-        dump_multiple_classification(model)
+        dump_multiple_classification(model,
+                                     # Operator cast-1 is not implemented in onnxruntime
+                                     allow_failure="StrictVersion(onnx.__version__) < StrictVersion('1.2')")
 
     def test_extra_trees_regressor(self):
         model = ExtraTreesRegressor(n_estimators=3)
