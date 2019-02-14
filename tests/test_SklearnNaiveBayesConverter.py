1be9eacc94a5284301f0811192e3d2d7785d2029
Prabhat Roy <prroy@microsoft.com>
2019-02-14 04:51:26 -0800

    Fixed Naives bayes converter/unit tests

--- tests/test_SklearnNaiveBayesConverter.py
+++ tests/test_SklearnNaiveBayesConverter.py
@@ -1,63 +1,78 @@
-import unittest
-import numpy
+import numpy as np
 import onnx
+import unittest
+from distutils.version import StrictVersion
+from sklearn.datasets import load_digits, load_iris
 from sklearn.naive_bayes import MultinomialNB, BernoulliNB
-from sklearn.datasets import load_iris
+
 from skl2onnx import convert_sklearn
 from skl2onnx.common.data_types import FloatTensorType
 from test_utils import dump_data_and_model
-from distutils.version import StrictVersion
 
 class TestNaiveBayesConverter(unittest.TestCase):
 
-    def _fit_model_binary_classification(self, model):
-        data = load_iris()
+    def _fit_model_binary_classification(self, model, data):
         X = data.data
         y = data.target
-        y[y == 2] = 1
+        mid_point = len(data.target_names) / 2
+        y[y < mid_point] = 0
+        y[y >= mid_point] = 1
         model.fit(X, y)
-        return model, X.astype(numpy.float32)
+        return model, X.astype(np.float32)
 
-    def _fit_model_multiclass_classification(self, model):
-        data = load_iris()
+    def _fit_model_multiclass_classification(self, model, data):
         X = data.data
         y = data.target
         model.fit(X, y)
-        return model, X.astype(numpy.float32)
+        return model, X.astype(np.float32)
 
-    @unittest.skipIf(StrictVersion(onnx.__version__) < StrictVersion('1.3'), 'Need Greater Opset 8')
     def test_model_multinomial_nb_binary_classification(self):
-        model, X = self._fit_model_binary_classification(MultinomialNB())
-        model_onnx = convert_sklearn(model, 'multinomial naive bayes', [('input', FloatTensorType([1, 4]))])
+        model, X = self._fit_model_binary_classification(MultinomialNB(),
+                                                         load_iris())
+        model_onnx = convert_sklearn(model, 'multinomial naive bayes',
+                                     [('input', FloatTensorType(X.shape))])
         self.assertIsNotNone(model_onnx)
-        dump_data_and_model(X, model, model_onnx, basename="SklearnBinMultinomialNB-OneOff",
-                            allow_failure="StrictVersion(onnxruntime.__version__) <= StrictVersion('0.1.3')")
+        dump_data_and_model(X, model, model_onnx,
+                        basename="SklearnBinMultinomialNB",
+                        allow_failure="StrictVersion(onnxruntime.__version__)"
+                                       "<= StrictVersion('0.1.3')")
 
-    @unittest.skip(reason="Type 'tensor(float)' of input parameter (input) of operator "
-                          "(ConstantOfShape) in node (ConstantOfShape) is invalid.")
+    @unittest.skipIf(StrictVersion(onnx.__version__) <= StrictVersion('1.3'),
+                     'Needs greater opset 9')
     def test_model_bernoulli_nb_binary_classification(self):
-        model, X = self._fit_model_binary_classification(BernoulliNB())
-        model_onnx = convert_sklearn(model, 'bernoulli naive bayes', [('input', FloatTensorType([1, 4]))])
+        model, X = self._fit_model_binary_classification(BernoulliNB(),
+                                                         load_digits())
+        model_onnx = convert_sklearn(model, 'bernoulli naive bayes',
+                                     [('input', FloatTensorType(X.shape))])
         self.assertIsNotNone(model_onnx)
-        dump_data_and_model(X[:5], model, model_onnx, basename="SklearnBinBernoulliNB-OneOff",
-                            allow_failure="StrictVersion(onnxruntime.__version__) <= StrictVersion('0.1.4')")
+        dump_data_and_model(X, model, model_onnx,
+                        basename="SklearnBinBernoulliNB",
+                        allow_failure="StrictVersion(onnxruntime.__version__)"
+                                       "<= StrictVersion('0.1.4')")
 
-    @unittest.skipIf(StrictVersion(onnx.__version__) <= StrictVersion('1.3'), 'Need Greater Opset 9')
     def test_model_multinomial_nb_multiclass(self):
-        model, X = self._fit_model_multiclass_classification(MultinomialNB())
-        model_onnx = convert_sklearn(model, 'multinomial naive bayes', [('input', FloatTensorType([1, 4]))])
+        model, X = self._fit_model_multiclass_classification(MultinomialNB(),
+                                                             load_iris())
+        model_onnx = convert_sklearn(model, 'multinomial naive bayes',
+                                     [('input', FloatTensorType(X.shape))])
         self.assertIsNotNone(model_onnx)
-        dump_data_and_model(X, model, model_onnx, basename="SklearnMclMultinomialNB-OneOff",
-                            allow_failure="StrictVersion(onnxruntime.__version__) <= StrictVersion('0.1.3')")
+        dump_data_and_model(X, model, model_onnx,
+                        basename="SklearnMclMultinomialNB",
+                        allow_failure="StrictVersion(onnxruntime.__version__)"
+                                       "<= StrictVersion('0.1.3')")
 
-    @unittest.skip(reason="Type 'tensor(float)' of input parameter (input) of operator (ConstantOfShape) "
-                          "in node (ConstantOfShape) is invalid.")
+    @unittest.skipIf(StrictVersion(onnx.__version__) <= StrictVersion('1.3'),
+                     'Needs greater opset 9')
     def test_model_bernoulli_nb_multiclass(self):
-        model, X = self._fit_model_multiclass_classification(BernoulliNB())
-        model_onnx = convert_sklearn(model, 'bernoulli naive bayes', [('input', FloatTensorType([1, 4]))])
+        model, X = self._fit_model_multiclass_classification(BernoulliNB(),
+                                                             load_digits())
+        model_onnx = convert_sklearn(model, 'bernoulli naive bayes',
+                                     [('input', FloatTensorType(X.shape))])
         self.assertIsNotNone(model_onnx)
-        dump_data_and_model(X, model, model_onnx, basename="SklearnMclBernoulliNB-OneOff",
-                            allow_failure="StrictVersion(onnxruntime.__version__) <= StrictVersion('0.1.3')")
+        dump_data_and_model(X, model, model_onnx,
+                        basename="SklearnMclBernoulliNB",
+                        allow_failure="StrictVersion(onnxruntime.__version__)"
+                                       "<= StrictVersion('0.1.3')")
 
 
 if __name__ == "__main__":
