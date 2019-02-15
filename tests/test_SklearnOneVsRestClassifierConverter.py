54870dcf45cf68d99bd204cbedba5a3833290e7a
xavier dupré <xavier.dupre@gmail.com>
2019-02-15 17:54:52 +0100

    require more recent onnxruntime

--- tests/test_SklearnOneVsRestClassifierConverter.py
+++ tests/test_SklearnOneVsRestClassifierConverter.py
@@ -10,15 +10,18 @@ class TestOneVsRestClassifierConverter(unittest.TestCase):
 
     def test_ova(self):
         model = OneVsRestClassifier(LogisticRegression())
-        dump_multiple_classification(model)
+        dump_multiple_classification(model,
+                                     allow_failure="StrictVersion(onnxruntime.__version__) <= StrictVersion('0.2.1')")
 
     def test_ova_02(self):
         model = OneVsRestClassifier(LogisticRegression())
-        dump_multiple_classification(model, first_class=2, suffix="F2")
+        dump_multiple_classification(model, first_class=2, suffix="F2",
+                                     allow_failure="StrictVersion(onnxruntime.__version__) <= StrictVersion('0.2.1')")
 
     def test_ova_string(self):
         model = OneVsRestClassifier(LogisticRegression())
-        dump_multiple_classification(model, verbose=False, label_string=True, suffix="String")
+        dump_multiple_classification(model, verbose=False, label_string=True, suffix="String",
+                                     allow_failure="StrictVersion(onnxruntime.__version__) <= StrictVersion('0.2.1')")
 
 
 if __name__ == "__main__":
