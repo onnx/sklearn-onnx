a5d6648a81c3005eb5918f0e0028db9b57f5edab
Prabhat Roy <prroy@microsoft.com>
2019-03-12 08:10:31 -0700

    Added MLPClassifier converter

---
+++ tests/test_SklearnMLPClassifierConverter.py
@@ -0,0 +1,45 @@
+"""
+Tests scikit-learn's MLPClassifier converter
+"""
+
+import numpy as np
+import unittest
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+from sklearn.neural_network import MLPClassifier
+
+from skl2onnx import convert_sklearn
+from skl2onnx.common.data_types import FloatTensorType
+from test_utils import dump_data_and_model
+
+
+class TestSklearnCalibratedClassifierCVConverters(unittest.TestCase):
+
+    def test_model_mlp_classifier_binary(self):
+        data = load_iris()
+        X, y = data.data, data.target
+        y[y > 1] = 1
+        X_train, X_test, y_train, y_test = train_test_split(
+            X, y, test_size=0.2, random_state=42)
+        model = MLPClassifier().fit(X_train, y_train)
+        model_onnx = convert_sklearn(model, 'scikit-learn MLPClassifier',
+                                     [('input', FloatTensorType([1, 4]))])
+        self.assertTrue(model_onnx is not None)
+        dump_data_and_model(X_test.astype(np.float32), model, model_onnx,
+                            basename="SklearnMLPClassifierBinary")
+
+    def test_model_mlp_classifier_multiclass(self):
+        data = load_iris()
+        X, y = data.data, data.target
+        X_train, X_test, y_train, y_test = train_test_split(
+            X, y, test_size=0.2, random_state=42)
+        model = MLPClassifier().fit(X_train, y_train)
+        model_onnx = convert_sklearn(model, 'scikit-learn MLPClassifier',
+                                     [('input', FloatTensorType([1, 4]))])
+        self.assertTrue(model_onnx is not None)
+        dump_data_and_model(X_test.astype(np.float32), model, model_onnx,
+                            basename="SklearnMLPClassifierMultiClass")
+
+
+if __name__ == "__main__":
+    unittest.main()
