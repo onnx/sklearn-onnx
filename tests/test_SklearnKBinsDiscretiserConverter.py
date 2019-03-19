5a39a1eaaa8d359cc37f510cffa655fd85913727
Prabhat Roy <prroy@microsoft.com>
2019-03-19 02:43:41 -0700

    Updated KBinsDiscretiser converter and tests

--- tests/test_SklearnKBinsDiscretiserConverter.py
+++ tests/test_SklearnKBinsDiscretiserConverter.py
@@ -6,17 +6,16 @@ import numpy as np
 import unittest
 from sklearn.preprocessing import KBinsDiscretizer
 from skl2onnx import convert_sklearn
-from skl2onnx.common.data_types import FloatTensorType
+from skl2onnx.common.data_types import FloatTensorType, Int64TensorType
 from test_utils import dump_data_and_model
 
 
 class TestSklearnKBinsDiscretiser(unittest.TestCase):
 
-    @unittest.skip(reason="Digitize op has not yet been implemented in onnx")
     def test_model_k_bins_discretiser_ordinal_uniform(self):
         X = np.array([[1.2, 3.2, 1.3, -5.6], [4.3, -3.2, 5.7, 1.0],
                       [0, 3.2, 4.7, -8.9]])
-        model = KBinsDiscretizer(nbins=3, encode='ordinal',
+        model = KBinsDiscretizer(n_bins=3, encode='ordinal',
                                  strategy='uniform').fit(X)
         model_onnx = convert_sklearn(model, 'scikit-learn KBinsDiscretiser',
                                      [('input', FloatTensorType(X.shape))])
@@ -24,11 +23,10 @@ class TestSklearnKBinsDiscretiser(unittest.TestCase):
         dump_data_and_model(X.astype(np.float32), model, model_onnx,
                 basename="SklearnKBinsDiscretiserOrdinalUniform")
 
-    @unittest.skip(reason="Digitize op has not yet been implemented in onnx")
     def test_model_k_bins_discretiser_ordinal_quantile(self):
         X = np.array([[1.2, 3.2, 1.3, -5.6], [4.3, -3.2, 5.7, 1.0],
                       [0, 3.2, 4.7, -8.9]])
-        model = KBinsDiscretizer(nbins=[3, 2, 3, 4], encode='ordinal',
+        model = KBinsDiscretizer(n_bins=[3, 2, 3, 4], encode='ordinal',
                                  strategy='quantile').fit(X)
         model_onnx = convert_sklearn(model, 'scikit-learn KBinsDiscretiser',
                                      [('input', FloatTensorType(X.shape))])
@@ -36,11 +34,10 @@ class TestSklearnKBinsDiscretiser(unittest.TestCase):
         dump_data_and_model(X.astype(np.float32), model, model_onnx,
                 basename="SklearnKBinsDiscretiserOrdinalQuantile")
 
-    @unittest.skip(reason="Digitize op has not yet been implemented in onnx")
     def test_model_k_bins_discretiser_ordinal_kmeans(self):
         X = np.array([[1.2, 3.2, 1.3, -5.6], [4.3, -3.2, 5.7, 1.0],
                       [0, 3.2, 4.7, -8.9]])
-        model = KBinsDiscretizer(nbins=3, encode='ordinal',
+        model = KBinsDiscretizer(n_bins=3, encode='ordinal',
                                  strategy='kmeans').fit(X)
         model_onnx = convert_sklearn(model, 'scikit-learn KBinsDiscretiser',
                                      [('input', FloatTensorType(X.shape))])
@@ -48,11 +45,10 @@ class TestSklearnKBinsDiscretiser(unittest.TestCase):
         dump_data_and_model(X.astype(np.float32), model, model_onnx,
                 basename="SklearnKBinsDiscretiserOrdinalKMeans")
 
-    @unittest.skip(reason="Digitize op has not yet been implemented in onnx")
     def test_model_k_bins_discretiser_onehot_dense_uniform(self):
         X = np.array([[1.2, 3.2, 1.3, -5.6], [4.3, -3.2, 5.7, 1.0],
                       [0, 3.2, 4.7, -8.9]])
-        model = KBinsDiscretizer(nbins=[3, 2, 3, 4], encode='onehot-dense',
+        model = KBinsDiscretizer(n_bins=[3, 2, 3, 4], encode='onehot-dense',
                                  strategy='uniform').fit(X)
         model_onnx = convert_sklearn(model, 'scikit-learn KBinsDiscretiser',
                                      [('input', FloatTensorType(X.shape))])
@@ -60,11 +56,10 @@ class TestSklearnKBinsDiscretiser(unittest.TestCase):
         dump_data_and_model(X.astype(np.float32), model, model_onnx,
                 basename="SklearnKBinsDiscretiserOneHotDenseUniform")
 
-    @unittest.skip(reason="Digitize op has not yet been implemented in onnx")
     def test_model_k_bins_discretiser_onehot_dense_quantile(self):
         X = np.array([[1.2, 3.2, 1.3, -5.6], [4.3, -3.2, 5.7, 1.0],
                       [0, 3.2, 4.7, -8.9]])
-        model = KBinsDiscretizer(nbins=[3, 2, 3, 4], encode='onehot-dense',
+        model = KBinsDiscretizer(n_bins=[3, 2, 3, 4], encode='onehot-dense',
                                  strategy='quantile').fit(X)
         model_onnx = convert_sklearn(model, 'scikit-learn KBinsDiscretiser',
                                      [('input', FloatTensorType(X.shape))])
@@ -72,17 +67,82 @@ class TestSklearnKBinsDiscretiser(unittest.TestCase):
         dump_data_and_model(X.astype(np.float32), model, model_onnx,
                 basename="SklearnKBinsDiscretiserOneHotDenseQuantile")
 
-    @unittest.skip(reason="Digitize op has not yet been implemented in onnx")
     def test_model_k_bins_discretiser_onehot_dense_kmeans(self):
-        X = np.array([[1.2, 3.2, 1.3, -5.6], [4.3, -3.2, 5.7, 1.0],
-                      [0, 3.2, 4.7, -8.9]])
-        model = KBinsDiscretizer(nbins=3, encode='onehot-dense',
+        X = np.array([[1, 3, 3, -6], [3, -2, 5, 0],
+                      [0, 2, 7, -9]])
+        model = KBinsDiscretizer(n_bins=3, encode='onehot-dense',
                                  strategy='kmeans').fit(X)
         model_onnx = convert_sklearn(model, 'scikit-learn KBinsDiscretiser',
                                      [('input', FloatTensorType(X.shape))])
         self.assertTrue(model_onnx is not None)
         dump_data_and_model(X.astype(np.float32), model, model_onnx,
                 basename="SklearnKBinsDiscretiserOneHotDenseKMeans")
+
+    def test_model_k_bins_discretiser_ordinal_uniform_int(self):
+        X = np.array([[1, 3, 3, -6], [3, -2, 5, 0],
+                      [0, 2, 7, -9]])
+        model = KBinsDiscretizer(n_bins=3, encode='ordinal',
+                                 strategy='uniform').fit(X)
+        model_onnx = convert_sklearn(model, 'scikit-learn KBinsDiscretiser',
+                                     [('input', Int64TensorType(X.shape))])
+        self.assertTrue(model_onnx is not None)
+        dump_data_and_model(X.astype(np.int64), model, model_onnx,
+                basename="SklearnKBinsDiscretiserOrdinalUniformInt")
+
+    def test_model_k_bins_discretiser_ordinal_quantile_int(self):
+        X = np.array([[1, 3, 3, -6], [3, -2, 5, 0],
+                      [0, 2, 7, -9]])
+        model = KBinsDiscretizer(n_bins=[3, 2, 3, 4], encode='ordinal',
+                                 strategy='quantile').fit(X)
+        model_onnx = convert_sklearn(model, 'scikit-learn KBinsDiscretiser',
+                                     [('input', Int64TensorType(X.shape))])
+        self.assertTrue(model_onnx is not None)
+        dump_data_and_model(X.astype(np.int64), model, model_onnx,
+                basename="SklearnKBinsDiscretiserOrdinalQuantileInt")
+
+    def test_model_k_bins_discretiser_ordinal_kmeans_int(self):
+        X = np.array([[1, 3, 3, -6], [3, -2, 5, 0],
+                      [0, 2, 7, -9]])
+        model = KBinsDiscretizer(n_bins=3, encode='ordinal',
+                                 strategy='kmeans').fit(X)
+        model_onnx = convert_sklearn(model, 'scikit-learn KBinsDiscretiser',
+                                     [('input', Int64TensorType(X.shape))])
+        self.assertTrue(model_onnx is not None)
+        dump_data_and_model(X.astype(np.int64), model, model_onnx,
+                basename="SklearnKBinsDiscretiserOrdinalKMeansInt")
+
+    def test_model_k_bins_discretiser_onehot_dense_uniform_int(self):
+        X = np.array([[1, 3, 3, -6], [3, -2, 5, 0],
+                      [0, 2, 7, -9]])
+        model = KBinsDiscretizer(n_bins=[3, 2, 3, 4], encode='onehot-dense',
+                                 strategy='uniform').fit(X)
+        model_onnx = convert_sklearn(model, 'scikit-learn KBinsDiscretiser',
+                                     [('input', Int64TensorType(X.shape))])
+        self.assertTrue(model_onnx is not None)
+        dump_data_and_model(X.astype(np.int64), model, model_onnx,
+                basename="SklearnKBinsDiscretiserOneHotDenseUniformInt")
+
+    def test_model_k_bins_discretiser_onehot_dense_quantile_int(self):
+        X = np.array([[1, 3, 3, -6], [3, -2, 5, 0],
+                      [0, 2, 7, -9]])
+        model = KBinsDiscretizer(n_bins=[3, 2, 3, 4], encode='onehot-dense',
+                                 strategy='quantile').fit(X)
+        model_onnx = convert_sklearn(model, 'scikit-learn KBinsDiscretiser',
+                                     [('input', Int64TensorType(X.shape))])
+        self.assertTrue(model_onnx is not None)
+        dump_data_and_model(X.astype(np.int64), model, model_onnx,
+                basename="SklearnKBinsDiscretiserOneHotDenseQuantileInt")
+
+    def test_model_k_bins_discretiser_onehot_dense_kmeans_int(self):
+        X = np.array([[1, 3, 3, -6], [3, -2, 5, 0],
+                      [0, 2, 7, -9]])
+        model = KBinsDiscretizer(n_bins=3, encode='onehot-dense',
+                                 strategy='kmeans').fit(X)
+        model_onnx = convert_sklearn(model, 'scikit-learn KBinsDiscretiser',
+                                     [('input', Int64TensorType(X.shape))])
+        self.assertTrue(model_onnx is not None)
+        dump_data_and_model(X.astype(np.int64), model, model_onnx,
+                basename="SklearnKBinsDiscretiserOneHotDenseKMeansInt")
 
 
 if __name__ == "__main__":
