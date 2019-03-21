16c626853dd455d2e370d07eb476b9bb3427dbe6
Prabhat Roy <prroy@microsoft.com>
2019-03-21 02:51:18 -0700

    Skip unit test for onnx==1.4.1

--- tests/test_SklearnNearestNeighbourConverter.py
+++ tests/test_SklearnNearestNeighbourConverter.py
@@ -37,25 +37,34 @@ class TestNearestNeighbourConverter(unittest.TestCase):
         model, X = self._fit_model(KNeighborsRegressor(n_neighbors=2))
         model_onnx = convert_sklearn(model, 'KNN regressor', [('input', FloatTensorType([1, 4]))])
         self.assertIsNotNone(model_onnx)
-        dump_data_and_model(X.astype(numpy.float32)[:7], model, model_onnx,
-                             basename="SklearnKNeighborsRegressor-OneOffArray",
-                             allow_failure="StrictVersion(onnxruntime.__version__) <= StrictVersion('0.2.1')")
+        dump_data_and_model(
+            X.astype(numpy.float32)[:7], model, model_onnx,
+            basename="SklearnKNeighborsRegressor-OneOffArray",
+            allow_failure="StrictVersion(onnxruntime.__version__) "
+            "<= StrictVersion('0.2.1') or "
+            "StrictVersion(onnx.__version__) == StrictVersion('1.4.1')")
 
     def test_model_knn_regressor2_1(self):
         model, X = self._fit_model(KNeighborsRegressor(n_neighbors=1), n_targets=2)
         model_onnx = convert_sklearn(model, 'KNN regressor', [('input', FloatTensorType([1, 4]))])
         self.assertIsNotNone(model_onnx)
-        dump_data_and_model(X.astype(numpy.float32)[:2], model, model_onnx,
-                            basename="SklearnKNeighborsRegressor2-OneOffArray",
-                            allow_failure="StrictVersion(onnxruntime.__version__) <= StrictVersion('0.2.1')")
+        dump_data_and_model(
+            X.astype(numpy.float32)[:2], model, model_onnx,
+            basename="SklearnKNeighborsRegressor2-OneOffArray",
+            allow_failure="StrictVersion(onnxruntime.__version__) "
+            "<= StrictVersion('0.2.1') or "
+            "StrictVersion(onnx.__version__) == StrictVersion('1.4.1')")
 
     def test_model_knn_regressor2_2(self):
         model, X = self._fit_model(KNeighborsRegressor(n_neighbors=2), n_targets=2)
         model_onnx = convert_sklearn(model, 'KNN regressor', [('input', FloatTensorType([1, 4]))])
         self.assertIsNotNone(model_onnx)
-        dump_data_and_model(X.astype(numpy.float32)[:2], model, model_onnx,
-                            basename="SklearnKNeighborsRegressor2-OneOffArray",
-                            allow_failure="StrictVersion(onnxruntime.__version__) <= StrictVersion('0.2.1')")
+        dump_data_and_model(
+            X.astype(numpy.float32)[:2], model, model_onnx,
+            basename="SklearnKNeighborsRegressor2-OneOffArray",
+            allow_failure="StrictVersion(onnxruntime.__version__) "
+            "<= StrictVersion('0.2.1') or "
+            "StrictVersion(onnx.__version__) == StrictVersion('1.4.1')")
 
     def test_model_knn_regressor_weights_distance(self):
         model, X = self._fit_model(KNeighborsRegressor(weights='distance'))
@@ -66,7 +75,8 @@ class TestNearestNeighbourConverter(unittest.TestCase):
             X.astype(numpy.float32)[:7], model, model_onnx,
             basename="SklearnKNeighborsRegressorWeightsDistance-OneOffArray",
             allow_failure="StrictVersion(onnxruntime.__version__) <= "
-                          "StrictVersion('0.2.1')")
+            "StrictVersion('0.2.1') or "
+            "StrictVersion(onnx.__version__) == StrictVersion('1.4.1')")
 
     def test_model_knn_regressor_metric_cityblock(self):
         model, X = self._fit_model(KNeighborsRegressor(metric='cityblock'))
@@ -77,27 +87,34 @@ class TestNearestNeighbourConverter(unittest.TestCase):
             X.astype(numpy.float32)[:7], model, model_onnx,
             basename="SklearnKNeighborsRegressorMetricCityblock-OneOffArray",
             allow_failure="StrictVersion(onnxruntime.__version__) <= "
-                          "StrictVersion('0.2.1')")
+            "StrictVersion('0.2.1') or "
+            "StrictVersion(onnx.__version__) == StrictVersion('1.4.1')")
 
     def test_model_knn_classifier_binary_class(self):
         model, X = self._fit_model_binary_classification(KNeighborsClassifier())
         model_onnx = convert_sklearn(model, 'KNN classifier binary', [('input', FloatTensorType([1, 3]))])
         self.assertIsNotNone(model_onnx)
-        dump_data_and_model(numpy.atleast_2d(X[0]).astype(numpy.float32)[:7],
-                            model, model_onnx,
-                            basename="SklearnKNeighborsClassifierBinary",
-                            allow_failure="StrictVersion(onnx.__version__) == StrictVersion('1.1.2') or "
-                                          "StrictVersion(onnxruntime.__version__) <= StrictVersion('0.2.1')")
+        dump_data_and_model(
+            numpy.atleast_2d(X[0]).astype(numpy.float32)[:7],
+            model, model_onnx,
+            basename="SklearnKNeighborsClassifierBinary",
+            allow_failure="StrictVersion(onnx.__version__) "
+            "== StrictVersion('1.1.2') or "
+            "StrictVersion(onnxruntime.__version__) <= StrictVersion('0.2.1') "
+            "or StrictVersion(onnx.__version__) == StrictVersion('1.4.1')")
 
     def test_model_knn_classifier_multi_class(self):
         model, X = self._fit_model_multiclass_classification(KNeighborsClassifier())
         model_onnx = convert_sklearn(model, 'KNN classifier multi-class', [('input', FloatTensorType([1, 3]))])
         self.assertIsNotNone(model_onnx)
-        dump_data_and_model(numpy.atleast_2d(X[0]).astype(numpy.float32)[:7],
-                            model, model_onnx,
-                            basename="SklearnKNeighborsClassifierMulti",
-                            allow_failure="StrictVersion(onnx.__version__) == StrictVersion('1.1.2') or "
-                                          "StrictVersion(onnxruntime.__version__) <= StrictVersion('0.2.1')")
+        dump_data_and_model(
+            numpy.atleast_2d(X[0]).astype(numpy.float32)[:7],
+            model, model_onnx,
+            basename="SklearnKNeighborsClassifierMulti",
+            allow_failure="StrictVersion(onnx.__version__) "
+            "== StrictVersion('1.1.2') or "
+            "StrictVersion(onnxruntime.__version__) <= StrictVersion('0.2.1') "
+            "or StrictVersion(onnx.__version__) == StrictVersion('1.4.1')")
 
 
 if __name__ == "__main__":
