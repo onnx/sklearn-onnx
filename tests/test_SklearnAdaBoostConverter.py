9d3de28ac2d52d2e7ff7eea93c4e7b5fab9eafdc
Prabhat <prabhatr@usc.edu>
2019-03-28 12:34:22 +0000

    Fixed AdaBoostRegressor converter (#96)
    * Fixed AdaBoostRegressor converter
    
    * Allow failure in onnx==1.4.1

--- tests/test_SklearnAdaBoostConverter.py
+++ tests/test_SklearnAdaBoostConverter.py
@@ -54,7 +54,6 @@ class TestSklearnAdaBoostModels(unittest.TestCase):
                                           "onnxruntime.__version__)"
                                           "<= StrictVersion('0.2.1')")
 
-    @unittest.skip(reason="CumSum op has not yet been implemented in onnx")
     def test_ada_boost_regressor(self):
         X, y = make_regression(n_features=4, n_samples=1000, random_state=42)
         X_train, X_test, y_train, y_test = train_test_split(
@@ -63,13 +62,15 @@ class TestSklearnAdaBoostModels(unittest.TestCase):
         model.fit(X_train, y_train)
         model_onnx = convert_sklearn(model, 'AdaBoost regression',
                                      [('input',
-                                      FloatTensorType(X_test.shape))])
+                                      FloatTensorType([1, 4]))])
         self.assertIsNotNone(model_onnx)
         dump_data_and_model(X_test.astype('float32'), model, model_onnx,
-                            basename="SklearnAdaBoostRegressor",
+                            basename="SklearnAdaBoostRegressor-OneOffArray",
                             allow_failure="StrictVersion("
-                                          "onnxruntime.__version__)"
-                                          "<= StrictVersion('0.2.1')")
+                            "onnxruntime.__version__) "
+                            "<= StrictVersion('0.2.1') or "
+                            "StrictVersion(onnx.__version__) "
+                            "== StrictVersion('1.4.1')")
 
 
 if __name__ == "__main__":
