55b954ff7c847deca73b09f6b85ca207bd001140
Vinitra Swamy <vinitras@gmail.com>
2019-04-05 14:02:39 -0700

    removing * import, importing each data type explicitly

--- skl2onnx/common/data_types.py
+++ skl2onnx/common/data_types.py
@@ -4,7 +4,11 @@
 # license information.
 # --------------------------------------------------------------------------
 
-from onnxtk.common.data_types import *
+from onnxtk.common.data_types import DataType, Int64Type, FloatType, StringType
+from onnxtk.common.data_types import TensorType, Int64TensorType
+from onnxtk.common.data_types import FloatTensorType, StringTensorType
+from onnxtk.common.data_types import DictionaryType, SequenceType
+from onnxtk.common.data_types import find_type_conversion
 
 def onnx_built_with_ml():
     """
