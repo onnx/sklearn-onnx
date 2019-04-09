f16979fe86f1123ef35712829cc03c56d95160ce
Vinitra Swamy <vinitras@gmail.com>
2019-04-09 11:37:25 -0700

    updating for data_type changes in onnxtk

--- skl2onnx/common/data_types.py
+++ skl2onnx/common/data_types.py
@@ -8,16 +8,4 @@ from onnxtk.common.data_types import DataType, Int64Type, FloatType  # noqa
 from onnxtk.common.data_types import StringType, TensorType, Int64TensorType  # noqa
 from onnxtk.common.data_types import FloatTensorType, StringTensorType  # noqa
 from onnxtk.common.data_types import DictionaryType, SequenceType  # noqa
-from onnxtk.common.data_types import find_type_conversion  # noqa
-
-
-def onnx_built_with_ml():
-    """
-    Tells if ONNX was built with flag ``ONNX-ML``.
-    """
-    seq = SequenceType(FloatTensorType())
-    try:
-        seq.to_onnx_type()
-        return True
-    except RuntimeError:
-        return False
+from onnxtk.common.data_types import find_type_conversion, onnx_built_with_ml  # noqa
