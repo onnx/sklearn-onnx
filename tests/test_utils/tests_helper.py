e1ba9768d4f20fcc90b6f4bfdd947106ae8b3ad7
xavier dupré <xavier.dupre@gmail.com>
2019-04-25 02:09:17 +0200

    check

--- tests/test_utils/tests_helper.py
+++ tests/test_utils/tests_helper.py
@@ -226,7 +226,11 @@ def dump_data_and_model(
         dest = os.path.join(folder, basename + ".model.pkl")
         names.append(dest)
         with open(dest, "wb") as f:
-            pickle.dump(model, f)
+            try:
+                pickle.dump(model, f)
+            except AttributeError as e:
+                print("[dump_data_and_model] cannot pickle model "
+                      "'{}' due to {}.".format(dest, e))
 
     if dump_error_log:
         error_dump = os.path.join(folder, basename + ".err")
