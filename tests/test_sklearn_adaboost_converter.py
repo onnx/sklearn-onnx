9ba1bf0a4aadaaaa005b376c7ce5ffa8d309503e
Prabhat <prabhat.roy@microsoft.com>
2019-06-06 20:09:47 +0100

    Support int feature type in non-linear regressor converters (#157)
    * Support int feature type in non-linear regressor converters
    
    * Added unit tests
    
    * Avoid cast with float inputs
    
    * Added random_state argument to mlp unit tests to avoid uncertainty

--- tests/test_sklearn_adaboost_converter.py
+++ tests/test_sklearn_adaboost_converter.py
@@ -6,12 +6,12 @@
 
 import unittest
 from sklearn.datasets import load_digits, load_iris
-from sklearn.datasets import make_regression
 from sklearn.ensemble import AdaBoostClassifier, AdaBoostRegressor
 from sklearn.model_selection import train_test_split
 from skl2onnx import convert_sklearn
-from skl2onnx.common.data_types import FloatTensorType, onnx_built_with_ml
-from test_utils import dump_data_and_model
+from skl2onnx.common.data_types import FloatTensorType, Int64TensorType
+from skl2onnx.common.data_types import onnx_built_with_ml
+from test_utils import dump_data_and_model, fit_regression_model
 
 
 class TestSklearnAdaBoostModels(unittest.TestCase):
@@ -70,19 +70,36 @@ class TestSklearnAdaBoostModels(unittest.TestCase):
         )
 
     def test_ada_boost_regressor(self):
-        X, y = make_regression(n_features=4, n_samples=1000, random_state=42)
-        X_train, X_test, y_train, y_test = train_test_split(
-            X, y, test_size=0.2, random_state=42)
-        model = AdaBoostRegressor(n_estimators=5)
-        model.fit(X_train, y_train)
-        model_onnx = convert_sklearn(model, "AdaBoost regression",
-                                     [("input", FloatTensorType([1, 4]))])
+        model, X = fit_regression_model(
+            AdaBoostRegressor(n_estimators=5))
+        model_onnx = convert_sklearn(
+            model, "AdaBoost regression",
+            [("input", FloatTensorType([1, X.shape[1]]))])
         self.assertIsNotNone(model_onnx)
         dump_data_and_model(
-            X_test.astype("float32"),
+            X,
             model,
             model_onnx,
             basename="SklearnAdaBoostRegressor-OneOffArray",
+            allow_failure="StrictVersion("
+            "onnxruntime.__version__) "
+            "<= StrictVersion('0.2.1') or "
+            "StrictVersion(onnx.__version__) "
+            "== StrictVersion('1.4.1')",
+        )
+
+    def test_ada_boost_regressor_int(self):
+        model, X = fit_regression_model(
+            AdaBoostRegressor(n_estimators=5), is_int=True)
+        model_onnx = convert_sklearn(
+            model, "AdaBoost regression",
+            [("input", Int64TensorType([1, X.shape[1]]))])
+        self.assertIsNotNone(model_onnx)
+        dump_data_and_model(
+            X,
+            model,
+            model_onnx,
+            basename="SklearnAdaBoostRegressorInt-OneOffArray",
             allow_failure="StrictVersion("
             "onnxruntime.__version__) "
             "<= StrictVersion('0.2.1') or "
