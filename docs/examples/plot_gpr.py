c5081d188ad13bfd8a4112f3d97de4264ba4055c
xavier dupré <xavier.dupre@gmail.com>
2019-07-26 11:21:52 +0200

    extend example on GPR

--- docs/examples/plot_gpr.py
+++ docs/examples/plot_gpr.py
@@ -24,19 +24,18 @@ Train a model
 A very basic example using *GaussianProcessRegressor*
 on the Boston dataset.
 """
-
-import skl2onnx
-import onnxruntime
-import onnx
+import pprint
+import numpy
 import sklearn
+from sklearn.datasets import load_boston
 from sklearn.gaussian_process import GaussianProcessRegressor
 from sklearn.gaussian_process.kernels import DotProduct, RBF
-import numpy
+from sklearn.model_selection import train_test_split
+import onnx
 import onnxruntime as rt
+import skl2onnx
 from skl2onnx.common.data_types import FloatTensorType, DoubleTensorType
 from skl2onnx import convert_sklearn
-from sklearn.datasets import load_boston
-from sklearn.model_selection import train_test_split
 
 bost = load_boston()
 X, y = bost.data, bost.target
@@ -73,10 +72,10 @@ except RuntimeError as e:
 # every node output has outputs with fixed dimensions
 # And that's not the case for this model.
 # We need to disable these checkings by replacing
-# the fixed dimensions by variables dimensions
-# or string (see next line).
+# the fixed dimensions by an empty value.
+# (see next line).
 
-initial_type = [('X', FloatTensorType(['d1', 'd2']))]
+initial_type = [('X', FloatTensorType([None, None]))]
 onx = convert_sklearn(gpr, initial_types=initial_type)
 
 sess = rt.InferenceSession(onx.SerializeToString())
@@ -115,7 +114,7 @@ print('min(Y)-max(Y):', min(y_test), max(y_test))
 # constant matrix such as the trained coefficients
 # will be dumped as doubles and not as floats anymore.
 
-initial_type = [('X', DoubleTensorType(['d1', 'd2']))]
+initial_type = [('X', DoubleTensorType([None, None]))]
 onx64 = convert_sklearn(gpr, initial_types=initial_type,
                         dtype=numpy.float64)
 
@@ -146,10 +145,65 @@ print("ONNX with floats:", size32)
 print("ONNX with doubles:", size64)
 
 #################################
+# return_std=True
+# +++++++++++++++
+#
+# `GaussianProcessRegressor <https://scikit-learn.org/stable/modules/
+# generated/sklearn.gaussian_process.GaussianProcessRegressor.html>`_
+# is one model which defined additional parameter to the predict function.
+# If call with ``return_std=True``, the class returns one more results
+# and that needs to be reflected into the generated ONNX graph.
+# The converter needs to know that an extended graph is required.
+# That's done through the option mechanism
+# (see :ref:`l-conv-options`).
+
+initial_type = [('X', DoubleTensorType([None, None]))]
+options = {GaussianProcessRegressor: {'return_std': True}}
+try:
+    onx64_std = convert_sklearn(gpr, initial_types=initial_type,
+                                dtype=numpy.float64, options=options)
+except RuntimeError as e:
+    print(e)
+
+######################################
+# This error highlights the fact that the *scikit-learn*
+# computes internal variables on first call to method predict.
+# The converter needs them to be initialized by calling method
+# predict at least once and then converting again.
+
+gpr.predict(X_test[:1], return_std=True)
+onx64_std = convert_sklearn(gpr, initial_types=initial_type,
+                            dtype=numpy.float64, options=options)
+
+sess64_std = rt.InferenceSession(onx64_std.SerializeToString())
+pred_onx64_std = sess64_std.run(None, {'X': X_test[:5]})
+
+pprint.pprint(pred_onx64_std)
+
+###############################
+# Let's compare with *scikit-learn* prediction.
+
+pprint.pprint(gpr.predict(X_test[:5], return_std=True))
+
+#######################################
+# It looks good. Let's do a better checks.
+
+
+pred_onx64_std = sess64_std.run(None, {'X': X_test})
+pred_std = gpr.predict(X_test, return_std=True)
+
+
+diff = numpy.sort(numpy.abs(numpy.squeeze(pred_onx64_std[1]) -
+                            numpy.squeeze(pred_std[1])))[-5:]
+print(diff)
+
+#################################
+# There are some discrepencies but it seems reasonable.
+#
 # **Versions used for this example**
 
 print("numpy:", numpy.__version__)
 print("scikit-learn:", sklearn.__version__)
 print("onnx: ", onnx.__version__)
-print("onnxruntime: ", onnxruntime.__version__)
+print("onnxruntime: ", rt.__version__)
 print("skl2onnx: ", skl2onnx.__version__)
