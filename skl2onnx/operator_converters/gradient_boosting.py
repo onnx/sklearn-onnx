Fixed gradient boosting binary classification mismatches

skl2onnx/operator_converters/gradient_boosting.py
@@ -32,40 +32,15 @@ def convert_sklearn_gradient_boosting_classifier(scope, operator, container):
     if op.init == 'zero':
         base_values = np.zeros(op.loss_.K)
     elif op.init is None:
-        if op.n_classes_ == 2:
-            # class_prior_ was introduced in scikit-learn 0.21.
-            if hasattr(op.init_, 'class_prior_'):
-                base_values = [op.init_.class_prior_[0]]
-            else:
-                base_values = [op.init_.prior]
-            if op.loss == 'deviance':
-                # See https://github.com/scikit-learn/scikit-learn/blob/
-                # master/sklearn/ensemble/_gb_losses.py#L666.
-                eps = np.finfo(np.float32).eps
-                base_values = np.clip(base_values, eps, 1 - eps)
-                base_values = np.log(base_values / (1 - base_values))
-            else:
-                raise NotImplementedError(
-                    "Loss '{0}' is not supported yet. You "
-                    "may raise an issue at "
-                    "https://github.com/onnx/sklearn-onnx/issues.".format(
-                        op.loss))
+        x0 = np.zeros((1, op.estimators_[0, 0].n_features_))
+        if hasattr(op, '_raw_predict_init'):
+            # sklearn >= 0.21
+            base_values = op._raw_predict_init(x0).ravel()
+        elif hasattr(op, '_init_decision_function'):
+            # sklearn >= 0.20 and sklearn < 0.21
+            base_values = op._init_decision_function(x0).ravel()
         else:
-            # class_prior_ was introduced in scikit-learn 0.21.
-            x0 = np.zeros((1, op.estimators_[0, 0].n_features_))
-            if hasattr(op, '_raw_predict_init'):
-                # sklearn >= 0.21
-                base_values = op._raw_predict_init(x0).ravel()
-            elif hasattr(op, '_init_decision_function'):
-                # sklearn >= 0.21
-                base_values = op._init_decision_function(x0).ravel()
-            else:
-                raise RuntimeError("scikit-learn < 0.19 is not supported.")
-
-            # if hasattr(op.init_, 'class_prior_'):
-            #     base_values = op.init_.class_prior_
-            # else:
-            #     base_values = op.init_.priors
+            raise RuntimeError("scikit-learn < 0.19 is not supported.")
     else:
         raise NotImplementedError(
             'Setting init to an estimator is not supported, you may raise an '

tests/test_sklearn_gradient_boosting_converters.py
@@ -4,10 +4,10 @@
 # license information.
 # --------------------------------------------------------------------------
 
+from distutils.version import StrictVersion
 from logging import getLogger
 import unittest
 import numpy as np
-from distutils.version import StrictVersion
 from pandas import DataFrame
 from sklearn.datasets import make_classification
 from sklearn.ensemble import (
@@ -15,14 +15,15 @@ from sklearn.ensemble import (
     GradientBoostingRegressor
 )
 from sklearn.model_selection import train_test_split
+from onnxruntime import InferenceSession, __version__
 from skl2onnx import convert_sklearn
 from skl2onnx.common.data_types import FloatTensorType, Int64TensorType
 from skl2onnx.common.data_types import onnx_built_with_ml
 from test_utils import dump_binary_classification, dump_multiple_classification
+from test_utils import fit_classification_model
 from test_utils import dump_data_and_model, fit_regression_model
-from onnxruntime import InferenceSession, __version__
 
-threshold = "0.5.0"
+THRESHOLD = "0.5.0"
 
 
 class TestSklearnGradientBoostingModels(unittest.TestCase):
@@ -34,7 +35,7 @@ class TestSklearnGradientBoostingModels(unittest.TestCase):
     @unittest.skipIf(not onnx_built_with_ml(),
                      reason="Requires ONNX-ML extension.")
     @unittest.skipIf(
-        StrictVersion(__version__) <= StrictVersion(threshold),
+        StrictVersion(__version__) <= StrictVersion(THRESHOLD),
         reason="Depends on PR #1015 onnxruntime.")
     def test_gradient_boosting_classifier1Deviance(self):
         model = GradientBoostingClassifier(n_estimators=1, max_depth=2)
@@ -65,7 +66,7 @@ class TestSklearnGradientBoostingModels(unittest.TestCase):
         dump_binary_classification(
             model, suffix="1Deviance",
             allow_failure="StrictVersion(onnxruntime.__version__)"
-                          " <= StrictVersion('%s')" % threshold)
+                          " <= StrictVersion('%s')" % THRESHOLD)
 
     @unittest.skipIf(not onnx_built_with_ml(),
                      reason="Requires ONNX-ML extension.")
@@ -74,7 +75,7 @@ class TestSklearnGradientBoostingModels(unittest.TestCase):
         dump_binary_classification(
             model, suffix="3",
             allow_failure="StrictVersion(onnxruntime.__version__)"
-                          " <= StrictVersion('%s')" % threshold)
+                          " <= StrictVersion('%s')" % THRESHOLD)
 
     @unittest.skipIf(not onnx_built_with_ml(),
                      reason="Requires ONNX-ML extension.")
@@ -83,7 +84,91 @@ class TestSklearnGradientBoostingModels(unittest.TestCase):
         dump_multiple_classification(
             model,
             allow_failure="StrictVersion(onnxruntime.__version__)"
-            "<= StrictVersion('%s')" % threshold,
+            "<= StrictVersion('%s')" % THRESHOLD,
+        )
+
+    @unittest.skipIf(not onnx_built_with_ml(),
+                     reason="Requires ONNX-ML extension.")
+    def test_gradient_boosting_binary_classification(self):
+        model, X = fit_classification_model(
+            GradientBoostingClassifier(n_estimators=3), 2)
+        model_onnx = convert_sklearn(
+            model,
+            "gradient boosting classifier",
+            [("input", FloatTensorType([None, X.shape[1]]))],
+            dtype=np.float32,
+        )
+        self.assertIsNotNone(model_onnx)
+        dump_data_and_model(
+            X,
+            model,
+            model_onnx,
+            basename="SklearnGradientBoostingBinaryClassifier",
+            allow_failure="StrictVersion(onnxruntime.__version__)"
+            "<= StrictVersion('0.2.1')",
+        )
+
+    @unittest.skipIf(not onnx_built_with_ml(),
+                     reason="Requires ONNX-ML extension.")
+    def test_gradient_boosting_binary_classification_init_zero(self):
+        model, X = fit_classification_model(
+            GradientBoostingClassifier(n_estimators=4, init='zero'), 2)
+        model_onnx = convert_sklearn(
+            model,
+            "gradient boosting classifier",
+            [("input", FloatTensorType([None, X.shape[1]]))],
+            dtype=np.float32,
+        )
+        self.assertIsNotNone(model_onnx)
+        dump_data_and_model(
+            X,
+            model,
+            model_onnx,
+            basename="SklearnGradientBoostingBinaryClassifierInitZero",
+            allow_failure="StrictVersion(onnxruntime.__version__)"
+            "<= StrictVersion('0.2.1')",
+        )
+
+    @unittest.skipIf(not onnx_built_with_ml(),
+                     reason="Requires ONNX-ML extension.")
+    def test_gradient_boosting_multiclass_classification(self):
+        model, X = fit_classification_model(
+            GradientBoostingClassifier(n_estimators=4), 5)
+        model_onnx = convert_sklearn(
+            model,
+            "gradient boosting classifier",
+            [("input", FloatTensorType([None, X.shape[1]]))],
+            dtype=np.float32,
+        )
+        self.assertIsNotNone(model_onnx)
+        dump_data_and_model(
+            X,
+            model,
+            model_onnx,
+            basename="SklearnGradientBoostingMultiClassClassifier",
+            allow_failure="StrictVersion(onnxruntime.__version__)"
+            "<= StrictVersion('0.2.1')",
+        )
+
+    @unittest.skipIf(not onnx_built_with_ml(),
+                     reason="Requires ONNX-ML extension.")
+    def test_gradient_boosting_multiclass_classification_init_zero(self):
+        model, X = fit_classification_model(
+            GradientBoostingClassifier(n_estimators=4, init='zero'), 2)
+        model_onnx = convert_sklearn(
+            model,
+            "gradient boosting classifier",
+            [("input", FloatTensorType([None, X.shape[1]]))],
+            dtype=np.float32,
+        )
+        self.assertIsNotNone(model_onnx)
+        dump_data_and_model(
+            X,
+            model,
+            model_onnx,
+            basename="SklearnGradientBoostingMultiClassClassifierInitZero",
+            allow_failure="StrictVersion(onnxruntime.__version__)"
+            "<= StrictVersion('0.2.1')",
         )
 
     def test_gradient_boosting_regressor_ls_loss(self):
@@ -196,21 +281,22 @@ class TestSklearnGradientBoostingModels(unittest.TestCase):
         )
 
     @unittest.skipIf(
-        StrictVersion(__version__) <= StrictVersion(threshold),
+        StrictVersion(__version__) <= StrictVersion(THRESHOLD),
         reason="Depends on PR #1015 onnxruntime.")
     def test_gradient_boosting_regressor_learning_rate(self):
         X, y = make_classification(
             n_features=100, n_samples=1000, n_classes=2, n_informative=8)
-        X_train, X_test, y_train, y_test = train_test_split(
+        X_train, X_test, y_train, _ = train_test_split(
             X, y, test_size=0.5, random_state=42)
         model = GradientBoostingClassifier().fit(X_train, y_train)
         onnx_model = convert_sklearn(
             model, 'lr2', [('input', FloatTensorType(X_test.shape))])
         sess = InferenceSession(onnx_model.SerializeToString())
         res = sess.run(None, input_feed={'input': X_test.astype(np.float32)})
-        r1 = np.mean(np.isclose(model.predict_proba(X_test),
-                     list(map(lambda x: list(map(lambda y: x[y], x)),
-                              res[1])), atol=1e-4))
+        r1 = np.mean(
+            np.isclose(model.predict_proba(X_test),
+                       list(map(lambda x: list(map(lambda y: x[y], x)),
+                                res[1])), atol=1e-4))
         r2 = np.mean(res[0] == model.predict(X_test))
         assert r1 == r2
 