Fixed gradient boosting binary classification mismatches (#255)
* Fixed gradient boosting binary classification mismatches

* Enabled skipped unit tests and fixed unit test errors

* Set appropriate number of classes in muti-class classification unit test

tests/test_sklearn_sgd_classifier_converter.py
@@ -70,7 +70,7 @@ class TestSGDClassifierConverter(unittest.TestCase):
             X.astype(np.float32),
             model,
             model_onnx,
-            basename="SklearnSGDClassifierBinaryLog",
+            basename="SklearnSGDClassifierBinaryLog-Dec4",
             allow_failure="StrictVersion(onnx.__version__)"
                           " < StrictVersion('1.2') or "
                           "StrictVersion(onnxruntime.__version__)"
@@ -116,7 +116,7 @@ class TestSGDClassifierConverter(unittest.TestCase):
             X.astype(np.float32),
             model,
             model_onnx,
-            basename="SklearnSGDClassifierBinaryLogL1NoIntercept",
+            basename="SklearnSGDClassifierBinaryLogL1NoIntercept-Dec4",
             allow_failure="StrictVersion(onnx.__version__)"
                           " < StrictVersion('1.2') or "
                           "StrictVersion(onnxruntime.__version__)"
@@ -379,7 +379,7 @@ class TestSGDClassifierConverter(unittest.TestCase):
             "scikit-learn SGD multi-class classifier",
             [("input", Int64TensorType([None, X.shape[1]]))],
         )
-        X = np.array([X[0], X[0]])
+        X = X[6:8]
         self.assertIsNotNone(model_onnx)
         dump_data_and_model(
             X,