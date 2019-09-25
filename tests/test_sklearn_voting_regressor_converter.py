d2b971fce9e85e9e5e5ab8c276a2c5a398c528ea
xavier dupré <xavier.dupre@gmail.com>
2019-09-25 16:57:41 +0200

    Update test_sklearn_voting_regressor_converter.py

--- tests/test_sklearn_voting_regressor_converter.py
+++ tests/test_sklearn_voting_regressor_converter.py
@@ -1,4 +1,4 @@
-"""Tests GLMRegressor converter."""
+"""Tests VotingRegressor converter."""
 
 import unittest
 import numpy
@@ -15,17 +15,7 @@ from skl2onnx import convert_sklearn
 from skl2onnx.common.data_types import (
     FloatTensorType, Int64TensorType
 )
-from test_utils import dump_data_and_model
-
-
-def _fit_model(model, is_int=False):
-    X, y = datasets.make_regression(n_features=4, random_state=0)
-    if is_int:
-        X = X.astype(numpy.int64)
-    X_train, X_test, y_train, _ = train_test_split(X, y, test_size=0.5,
-                                                   random_state=42)
-    model.fit(X_train, y_train)
-    return model, X_test
+from test_utils import dump_data_and_model, fit_regression_model
 
 
 def model_to_test():
@@ -40,7 +30,7 @@ class TestVotingRegressorConverter(unittest.TestCase):
     @unittest.skipIf(VotingRegressor is None,
                      reason="new in 0.21")
     def test_model_voting_regression(self):
-        model, X = _fit_model(model_to_test())
+        model, X = fit_regression_model(model_to_test())
         model_onnx = convert_sklearn(
             model, "voting regression",
             [("input", FloatTensorType([None, X.shape[1]]))])
@@ -59,7 +49,7 @@ class TestVotingRegressorConverter(unittest.TestCase):
     @unittest.skipIf(VotingRegressor is None,
                      reason="new in 0.21")
     def test_model_voting_regression_int(self):
-        model, X = _fit_model(model_to_test(), is_int=True)
+        model, X = fit_regression_model(model_to_test(), is_int=True)
         model_onnx = convert_sklearn(
             model, "voting regression",
             [("input", Int64TensorType([None, X.shape[1]]))])
