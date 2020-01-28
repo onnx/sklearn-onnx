437818ada2414a6d9424f47f6ffd0b335764e854
xavier dupré <xavier.dupre@gmail.com>
2020-01-28 16:32:07 +0100

    Add test to check conversion is failing when the converter is missing

--- tests/test_sklearn_calibrated_classifier_cv_converter.py
+++ tests/test_sklearn_calibrated_classifier_cv_converter.py
@@ -7,9 +7,11 @@ from distutils.version import StrictVersion
 import numpy as np
 from sklearn.calibration import CalibratedClassifierCV
 from sklearn.datasets import load_digits, load_iris
+from sklearn.ensemble import RandomForestClassifier
 from sklearn.linear_model import LogisticRegression
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.neighbors import KNeighborsClassifier
+from sklearn.svm import SVC
 import onnxruntime
 from skl2onnx import convert_sklearn
 from skl2onnx.common.data_types import FloatTensorType, Int64TensorType
@@ -154,6 +156,47 @@ class TestSklearnCalibratedClassifierCVConverters(unittest.TestCase):
             allow_failure="StrictVersion(onnxruntime.__version__)"
             "<= StrictVersion('0.2.1')",
         )
+
+    @unittest.skipIf(not onnx_built_with_ml(),
+                     reason="Requires ONNX-ML extension.")
+    @unittest.skipIf(
+        StrictVersion(onnxruntime.__version__) < StrictVersion("0.5.0"),
+        reason="not available")
+    def test_model_calibrated_classifier_cv_rf(self):
+        data = load_iris()
+        X, y = data.data, data.target
+        y[y > 1] = 1
+        model = CalibratedClassifierCV(
+            base_estimator=RandomForestClassifier(n_estimators=2),
+            method='sigmoid').fit(X, y)
+        try:
+            convert_sklearn(
+                model, "unused",
+                [("input", FloatTensorType([None, X.shape[1]]))])
+            raise AssertionError(
+                "RandomForestClassifier has no decision_function")
+        except RuntimeError as e:
+            assert "cannot implement decision_function" in str(e)
+
+    @unittest.skipIf(not onnx_built_with_ml(),
+                     reason="Requires ONNX-ML extension.")
+    @unittest.skipIf(
+        StrictVersion(onnxruntime.__version__) < StrictVersion("0.5.0"),
+        reason="not available")
+    def test_model_calibrated_classifier_cv_svc(self):
+        data = load_iris()
+        X, y = data.data, data.target
+        model = CalibratedClassifierCV(
+            base_estimator=SVC(),
+            method='sigmoid').fit(X, y)
+        try:
+            convert_sklearn(
+                model, "unused",
+                [("input", FloatTensorType([None, X.shape[1]]))])
+            raise AssertionError(
+                "SVC has no decision_function for ovr")
+        except NotImplementedError as e:
+            assert "Converter is not implemented" in str(e)
 
 
 if __name__ == "__main__":
