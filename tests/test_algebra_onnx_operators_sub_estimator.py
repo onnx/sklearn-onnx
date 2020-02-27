040dedb665f56a7d8b414494e20e92316a9e7456
xavier dupré <xavier.dupre@gmail.com>
2020-02-27 23:17:39 +0100

    pep8

--- tests/test_algebra_onnx_operators_sub_estimator.py
+++ tests/test_algebra_onnx_operators_sub_estimator.py
@@ -3,7 +3,6 @@ import inspect
 from distutils.version import StrictVersion
 import numpy as np
 from numpy.testing import assert_almost_equal
-from onnx.defs import onnx_opset_version
 from sklearn.base import BaseEstimator, ClassifierMixin, clone
 from sklearn.datasets import load_iris
 from sklearn.linear_model import LogisticRegression
