402d91015cd3c387256c1558852069eddcd9e4da
xavier dupré <xavier.dupre@gmail.com>
2020-04-29 10:48:45 +0200

    Update test_sklearn_mlp_converter.py

--- tests/test_sklearn_mlp_converter.py
+++ tests/test_sklearn_mlp_converter.py
@@ -3,7 +3,6 @@ Tests scikit-learn's MLPClassifier and MLPRegressor converters.
 """
 
 import unittest
-from functools import wraps
 import numpy as np
 from numpy.testing import assert_almost_equal
 from sklearn.neural_network import MLPClassifier, MLPRegressor
@@ -15,24 +14,8 @@ except ImportError:
     try:
         from sklearn.utils.testing import ignore_warnings
     except ImportError:
-        class _IgnoreWarnings:
-            def __init__(self):
-                pass
-
-            def __call__(self, fn):
-                @wraps(fn)
-                def wrapper(*args, **kwargs):
-                    return fn(*args, **kwargs)
-                return wrapper
-
-            def __enter__(self):
-                pass
-
-            def __exit__(self):
-                pass
-
-        def ignore_warnings(obj=None, category=Warning):
-            return _IgnoreWarnings()(obj)
+        def ignore_warnings(category=Warning):
+            return lambda x: x
 
 from sklearn.exceptions import ConvergenceWarning
 from onnxruntime import InferenceSession
