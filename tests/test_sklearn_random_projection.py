720865945bbdb67d14465dca7b9038fe8735a672
xavier dupré <xavier.dupre@gmail.com>
2020-05-04 17:42:49 +0200

    Update test_sklearn_random_projection.py

--- tests/test_sklearn_random_projection.py
+++ tests/test_sklearn_random_projection.py
@@ -5,16 +5,20 @@
 # --------------------------------------------------------------------------
 
 import unittest
+from distutils.version import StrictVersion
 import numpy as np
+import onnxruntime
 from sklearn.random_projection import GaussianRandomProjection
 from skl2onnx import convert_sklearn, to_onnx
 from skl2onnx.common.data_types import FloatTensorType
 from test_utils import dump_data_and_model, TARGET_OPSET
 
+nort = StrictVersion(onnxruntime.__version__) < StrictVersion('0.5.0')
+
 
 class TestSklearnRandomProjection(unittest.TestCase):
 
-    @unittest.skipIf(TARGET_OPSET < 9, reason="MatMul not available")
+    @unittest.skipIf(TARGET_OPSET < 9 or nort, reason="MatMul not available")
     def test_gaussian_random_projection_float32(self):
         rng = np.random.RandomState(42)
         pt = GaussianRandomProjection(n_components=4)
@@ -23,18 +27,20 @@ class TestSklearnRandomProjection(unittest.TestCase):
         assert model.transform(X).shape[1] == 4
         model_onnx = convert_sklearn(
             model, "scikit-learn GaussianRandomProjection",
-            [("inputs", FloatTensorType([None, X.shape[1]]))])
+            [("inputs", FloatTensorType([None, X.shape[1]]))],
+            target_opset=TARGET_OPSET)
         self.assertIsNotNone(model_onnx)
         dump_data_and_model(X.astype(np.float32), model,
                             model_onnx, basename="GaussianRandomProjection")
 
-    @unittest.skipIf(TARGET_OPSET < 9, reason="MatMul not available")
+    @unittest.skipIf(TARGET_OPSET < 9 or nort, reason="MatMul not available")
     def test_gaussian_random_projection_float64(self):
         rng = np.random.RandomState(42)
         pt = GaussianRandomProjection(n_components=4)
         X = rng.rand(10, 5).astype(np.float64)
         model = pt.fit(X)
-        model_onnx = to_onnx(model, X[:1], dtype=np.float64)
+        model_onnx = to_onnx(model, X[:1], dtype=np.float64,
+                             target_opset=TARGET_OPSET)
         self.assertIsNotNone(model_onnx)
         dump_data_and_model(X, model,
                             model_onnx, basename="GaussianRandomProjection64")
