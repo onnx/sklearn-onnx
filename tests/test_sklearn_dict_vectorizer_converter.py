c0e13f0d1386f4f5105f6c8464186cbf1c446fd0
xavier dupré <xavier.dupre@gmail.com>
2020-05-22 15:30:33 +0200

    Addresses #198, issue with DictVectorizer

--- tests/test_sklearn_dict_vectorizer_converter.py
+++ tests/test_sklearn_dict_vectorizer_converter.py
@@ -21,24 +21,19 @@ class TestSklearnDictVectorizerConverter(unittest.TestCase):
         data = [{"amy": 1.0, "chin": 200.0}, {"nice": 3.0, "amy": 1.0}]
         model.fit_transform(data)
         model_onnx = convert_sklearn(
-            model,
-            "dictionary vectorizer",
+            model, "dictionary vectorizer",
             [(
                 "input",
                 DictionaryType(StringTensorType([1]), FloatTensorType([1])),
-            )],
-        )
+            )])
         self.assertTrue(model_onnx is not None)
         dump_data_and_model(
-            data,
-            model,
-            model_onnx,
+            data, model, model_onnx,
             basename="SklearnDictVectorizer-OneOff-SkipDim1",
             allow_failure="StrictVersion(onnxruntime.__version__)"
                           " <= StrictVersion('0.1.3') or "
                           "StrictVersion(onnx.__version__)"
-                          " < StrictVersion('1.3.0')",
-        )
+                          " < StrictVersion('1.3.0')")
 
     @unittest.skipIf(not onnx_built_with_ml(),
                      reason="Requires ONNX-ML extension.")
@@ -65,6 +60,18 @@ class TestSklearnDictVectorizerConverter(unittest.TestCase):
                           "StrictVersion(onnx.__version__)"
                           " < StrictVersion('1.3.0')",
         )
+
+    @unittest.skipIf(not onnx_built_with_ml(),
+                     reason="Requires ONNX-ML extension.")
+    def test_model_dict_vectorizer_issue(self):
+        key_value_map = [{1: 'A', 2: 'B'}, {1: 'C', 3: 'D'},
+                         {1: 'C', 3: 'A'}]
+        model = DictVectorizer(sparse=False).fit(key_value_map)
+        with self.assertRaises(RuntimeError):
+            convert_sklearn(
+                model, 'dv',
+                [("input", DictionaryType(Int64TensorType([1]),
+                  StringTensorType([1])))])
 
 
 if __name__ == "__main__":
