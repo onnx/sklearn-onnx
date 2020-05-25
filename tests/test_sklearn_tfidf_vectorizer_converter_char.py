50d0e7afa9f566af20738db55978754bbdcec6e0
Xavier Dupré <xadupre@users.noreply.github.com>
2020-05-25 12:52:00 +0200

    Fixes #197, fixes TfIdfVectorizer with analyser='char' (#471)
    * Investigate #197.
    * Update test_sklearn_tfidf_vectorizer_converter_dataset.py
    * Better error messages
    
    Signed-off-by: xavier dupré <xavier.dupre@gmail.com>

--- tests/test_sklearn_tfidf_vectorizer_converter_char.py
+++ tests/test_sklearn_tfidf_vectorizer_converter_char.py
@@ -78,6 +78,29 @@ class TestSklearnTfidfVectorizerRegex(unittest.TestCase):
     @unittest.skipIf(
         StrictVersion(onnx.__version__) <= StrictVersion("1.4.1"),
         reason="Requires opset 9.")
+    @unittest.skipIf(True, reason="expected failure")
+    def test_model_tfidf_vectorizer11_char_doublespace(self):
+        corpus = numpy.array([
+                'This is the first  document.',
+                'This document is the second document.',
+                ]).reshape((2, 1))
+        vect = TfidfVectorizer(ngram_range=(1, 1), norm=None,
+                               analyzer='char')
+        vect.fit(corpus.ravel())
+        model_onnx = convert_sklearn(vect, 'TfidfVectorizer',
+                                     [('input', StringTensorType([1]))])
+        self.assertTrue(model_onnx is not None)
+
+        dump_data_and_model(
+            corpus, vect, model_onnx,
+            basename="SklearnTfidfVectorizer11CharSpace-OneOff-SklCol",
+            allow_failure="StrictVersion(onnxruntime.__version__) <= "
+                          "StrictVersion('0.3.0')",
+            verbose=False)
+
+    @unittest.skipIf(
+        StrictVersion(onnx.__version__) <= StrictVersion("1.4.1"),
+        reason="Requires opset 9.")
     def test_model_tfidf_vectorizer12_char(self):
         corpus = numpy.array([
                 'This is the first document.',
