5f60f2c3a286a28cbb5418cdc68bb8158a0ead19
xavier dupré <xavier.dupre@gmail.com>
2020-06-09 16:32:44 +0200

    add option div

--- skl2onnx/operator_converters/scaler_op.py
+++ skl2onnx/operator_converters/scaler_op.py
@@ -7,15 +7,17 @@
 import numpy as np
 from sklearn.preprocessing import MaxAbsScaler, MinMaxScaler
 from sklearn.preprocessing import RobustScaler, StandardScaler
+from ..algebra.onnx_ops import OnnxSub, OnnxDiv
 from ..common._registration import register_converter
-from ..common._apply_operation import apply_cast
-from ..proto import onnx_proto
 from .common import concatenate_variables
 
 
 def convert_sklearn_scaler(scope, operator, container):
     # If there are multiple input variables, we need to combine them as a
     # whole tensor. Integer(s) would be converted to float(s).
+    # Options div use true division instead of Scaler operator
+    # which replaces a division by a multiplication.
+    # This leads to discrepencies in some cases.
     if len(operator.inputs) > 1:
         feature_name = concatenate_variables(scope, operator.inputs, container)
     else:
@@ -24,22 +26,27 @@ def convert_sklearn_scaler(scope, operator, container):
     op = operator.raw_operator
     op_type = 'Scaler'
     attrs = {'name': scope.get_unique_operator_name(op_type)}
+
     if isinstance(op, StandardScaler):
         C = operator.inputs[0].type.shape[1]
         attrs['offset'] = op.mean_ if op.with_mean else [0.0] * C
         attrs['scale'] = 1.0 / op.scale_ if op.with_std else [1.0] * C
+        inv_scale = op.scale_ if op.with_std else None
     elif isinstance(op, RobustScaler):
         C = operator.inputs[0].type.shape[1]
         attrs['offset'] = op.center_ if op.with_centering else [0.0] * C
         attrs['scale'] = 1.0 / op.scale_ if op.with_scaling else [1.0] * C
+        inv_scale = op.scale_ if op.with_scaling else None
     elif isinstance(op, MinMaxScaler):
         attrs['scale'] = op.scale_
         # Add 1e-8 to avoid divided by 0
         attrs['offset'] = -op.min_/(op.scale_ + 1e-8)
+        inv_scale = None
     elif isinstance(op, MaxAbsScaler):
         C = operator.inputs[0].type.shape[1]
         attrs['scale'] = 1.0 / op.scale_
         attrs['offset'] = [0.] * C
+        inv_scale = op.scale_
     else:
         raise ValueError('Only scikit-learn StandardScaler and RobustScaler '
                          'are supported but got %s. You may raise '
@@ -53,35 +60,26 @@ def convert_sklearn_scaler(scope, operator, container):
         if isinstance(v, np.ndarray) and v.dtype == np.float32:
             attrs[k] = v.astype(np.float64)
 
-    options = container.get_options(op, dict(double=False))
-    cast = options['double'] and container.dtype != np.float64
-    if cast:
-        feature_name_double = scope.get_unique_variable_name(
-            'scaler_double')
-        apply_cast(scope, feature_name, feature_name_double,
-                   container, to=onnx_proto.TensorProto.DOUBLE)
-        feature_name = feature_name_double
+    if inv_scale is not None:
+        options = container.get_options(op, dict(div=False))
+        div = options['div']
+        if div:
+            opv = container.target_opset
+            sub = OnnxSub(feature_name, attrs['offset'], op_version=opv)
+            div = OnnxDiv(sub, inv_scale, op_version=opv,
+                          output_names=[operator.outputs[0].full_name])
+            div.add_to(scope, container)
+            return
 
-        scaled_feature_name_double = scope.get_unique_variable_name(
-            'scaled_double')
-        container.add_node(
-            op_type, feature_name, scaled_feature_name_double,
-            op_domain='ai.onnx.ml', **attrs)
-
-        apply_cast(scope, scaled_feature_name_double,
-                   operator.outputs[0].full_name,
-                   container, to=container.proto_dtype)
-    else:
-        container.add_node(
-            op_type, feature_name, operator.outputs[0].full_name,
-            op_domain='ai.onnx.ml', **attrs)
+    container.add_node(
+        op_type, feature_name, operator.outputs[0].full_name,
+        op_domain='ai.onnx.ml', **attrs)
 
 
 register_converter('SklearnRobustScaler', convert_sklearn_scaler,
-                   options={'double': [False, True]})
+                   options={'div': [False, True]})
 register_converter('SklearnScaler', convert_sklearn_scaler,
-                   options={'double': [False, True]})
-register_converter('SklearnMinMaxScaler', convert_sklearn_scaler,
-                   options={'double': [False, True]})
+                   options={'div': [False, True]})
+register_converter('SklearnMinMaxScaler', convert_sklearn_scaler)
 register_converter('SklearnMaxAbsScaler', convert_sklearn_scaler,
-                   options={'double': [False, True]})
+                   options={'div': [False, True]})
