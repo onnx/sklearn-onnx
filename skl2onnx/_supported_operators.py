d415878f16144111f3a2a89cf01331d3129b1b6a
xavier dupré <xavier.dupre@gmail.com>
2020-06-11 14:27:58 +0200

    predict

--- skl2onnx/_supported_operators.py
+++ skl2onnx/_supported_operators.py
@@ -13,8 +13,9 @@ from sklearn.calibration import CalibratedClassifierCV
 from sklearn.linear_model import (
     LogisticRegression, LogisticRegressionCV,
     PassiveAggressiveClassifier,
-    Perceptron, SGDClassifier,
+    Perceptron,
     RidgeClassifier, RidgeClassifierCV,
+    SGDClassifier,
 )
 from sklearn.svm import LinearSVC, OneClassSVM
 
@@ -75,7 +76,9 @@ from sklearn.tree import (
 )
 
 # Gaussian processes
-from sklearn.gaussian_process import GaussianProcessRegressor
+from sklearn.gaussian_process import (
+    GaussianProcessClassifier, GaussianProcessRegressor
+)
 
 # GridSearchCV
 from sklearn.model_selection import GridSearchCV
@@ -218,6 +221,7 @@ sklearn_classifier_list = list(filter(lambda m: m is not None, [
     ExtraTreeClassifier,
     ExtraTreesClassifier,
     GaussianNB,
+    GaussianProcessClassifier,
     GradientBoostingClassifier,
     HistGradientBoostingClassifier,
     KNeighborsClassifier,
@@ -273,6 +277,7 @@ def build_sklearn_operator_name_map():
                 ExtraTreesRegressor,
                 FunctionTransformer,
                 GaussianMixture,
+                GaussianProcessClassifier,
                 GaussianProcessRegressor,
                 GaussianRandomProjection,
                 GenericUnivariateSelect,
