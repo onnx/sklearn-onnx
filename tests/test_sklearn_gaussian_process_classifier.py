80342adc6f3d7bec9e4941b895040f4e85436945
xavier dupré <xavier.dupre@gmail.com>
2020-06-12 10:05:28 +0200

    finalize the model
    Signed-off-by: xavier dupré <xavier.dupre@gmail.com>

--- tests/test_sklearn_gaussian_process_classifier.py
+++ tests/test_sklearn_gaussian_process_classifier.py
@@ -5,26 +5,11 @@
 # --------------------------------------------------------------------------
 
 import unittest
-import inspect
-from io import StringIO
 from distutils.version import StrictVersion
 import numpy as np
-import pandas as pd
-from numpy.testing import assert_almost_equal
-from sklearn.datasets import load_iris
 from sklearn.gaussian_process import GaussianProcessClassifier
-from sklearn.gaussian_process.kernels import (
-    Sum, DotProduct, ExpSineSquared, RationalQuadratic,
-    RBF, ConstantKernel as C,
-)
-from sklearn.model_selection import train_test_split
 from skl2onnx.common.data_types import FloatTensorType, DoubleTensorType
 from skl2onnx import to_onnx
-from skl2onnx.proto import get_latest_tested_opset_version
-from skl2onnx.operator_converters.gaussian_process import (
-    convert_kernel, convert_kernel_diag
-)
-from onnxruntime import InferenceSession
 from onnxruntime import __version__ as ort_version
 from test_utils import (
     dump_data_and_model, fit_classification_model, TARGET_OPSET)
@@ -32,10 +17,8 @@ from test_utils import (
 
 class TestSklearnGaussianProcessClassifier(unittest.TestCase):
 
-    @unittest.skipIf(
-        StrictVersion(ort_version) <= StrictVersion("0.4.0"),
-        reason="onnxruntime too old")
-    def test_gpr_rbf_fitted_true(self):
+    @unittest.skipIf(TARGET_OPSET < 12, reason="einsum")
+    def test_gpc(self):
 
         gp = GaussianProcessClassifier()
         gp, X = fit_classification_model(gp, n_classes=2)
@@ -43,11 +26,18 @@ class TestSklearnGaussianProcessClassifier(unittest.TestCase):
         # return_cov=False, return_std=False
         model_onnx = to_onnx(
             gp, initial_types=[('X', DoubleTensorType([None, None]))],
-            dtype=np.float64, target_opset=TARGET_OPSET)
+            dtype=np.float64, target_opset=TARGET_OPSET,
+            options={GaussianProcessClassifier: {
+                'zipmap': False, 'optim': 'cdist'}})
         self.assertTrue(model_onnx is not None)
-        dump_data_and_model(X, gp, model_onnx,
-                            verbose=False,
-                            basename="SklearnGaussianProcessRBFT")
+        
+        from mlprodict.onnxrt import OnnxInference
+        oinf = OnnxInference(model_onnx)
+        oinf.run({'X': X.astype(np.float64)}, verbose=1, fLOG=print)
+        
+        dump_data_and_model(
+            X.astype(numpy.float64), gp, model_onnx,
+            verbose=False, basename="SklearnGaussianProcessRBFT")
 
 
 if __name__ == "__main__":
