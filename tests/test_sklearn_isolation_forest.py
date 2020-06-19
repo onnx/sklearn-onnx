17d58abc171404e91e037b2e9fd01ddb541a3eea
xavier dupré <xavier.dupre@gmail.com>
2020-06-19 09:34:33 +0200

    finalize converter for isolation forest

--- tests/test_sklearn_isolation_forest.py
+++ tests/test_sklearn_isolation_forest.py
@@ -5,7 +5,6 @@ import unittest
 import numpy as np
 from sklearn.ensemble import IsolationForest
 from skl2onnx import to_onnx
-from skl2onnx.common.data_types import FloatTensorType
 from test_utils import dump_data_and_model, TARGET_OPSET
 
 
@@ -13,7 +12,19 @@ class TestSklearnIsolationForest(unittest.TestCase):
 
     def test_isolation_forest(self):
         isol = IsolationForest(n_estimators=3, random_state=0)
-        data = np.array([[-1.1], [0.3], [0.5], [100]], dtype=np.float32)
+        data = np.array([[-1.1, -1.2], [0.3, 0.2],
+                         [0.5, 0.4], [100., 99.]], dtype=np.float32)
+        model = isol.fit(data)
+        model_onnx = to_onnx(model, data, target_opset=TARGET_OPSET)
+        self.assertIsNotNone(model_onnx)
+        dump_data_and_model(data, model, model_onnx,
+                            basename="IsolationForest")
+
+    def test_isolation_forest_big(self):
+        isol = IsolationForest(n_estimators=5, random_state=0)
+        data = np.random.rand(100, 4).astype(np.float32)
+        data[-1, 2:] = 99.
+        data[-2, :2] = -99.
         model = isol.fit(data)
         model_onnx = to_onnx(model, data, target_opset=TARGET_OPSET)
         self.assertIsNotNone(model_onnx)
