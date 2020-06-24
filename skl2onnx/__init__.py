ec0b6d7c3f1ef49ef08780161b020f768b3f1cd6
xavier dupré <xavier.dupre@gmail.com>
2020-06-24 15:22:41 +0200

    Update __init__.py

--- skl2onnx/__init__.py
+++ skl2onnx/__init__.py
@@ -7,7 +7,7 @@
 """
 Main entry point to the converter from the *scikit-learn* to *onnx*.
 """
-__version__ = "1.7.1074"
+__version__ = "1.7.1075"
 __author__ = "Microsoft"
 __producer__ = "skl2onnx"
 __producer_version__ = __version__
