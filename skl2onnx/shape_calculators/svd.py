ba63235c856f155ddd4e0446cfd7d00864a41bea
Xavier Dupré <xadupre@users.noreply.github.com>
2020-08-10 22:25:09 +0200

    Fixes #503, support sub operators in algebra (#504)
    * Fix #503, support sub operator in algebra
    * Update test_algebra_onnx_operators_wrapped.py
    
    Signed-off-by: xavier dupré <xavier.dupre@gmail.com>

--- skl2onnx/shape_calculators/svd.py
+++ skl2onnx/shape_calculators/svd.py
@@ -6,8 +6,7 @@
 
 from ..common._registration import register_shape_calculator
 from ..common.data_types import (
-    FloatTensorType, Int64TensorType, DoubleTensorType
-)
+    FloatTensorType, Int64TensorType, DoubleTensorType)
 from ..common.utils import check_input_and_output_numbers
 from ..common.utils import check_input_and_output_types
 
@@ -29,12 +28,15 @@ def calculate_sklearn_truncated_svd_output_shapes(operator):
     if len(operator.inputs[0].type.shape) != 2:
         raise RuntimeError('Only 2-D tensor(s) can be input(s).')
 
+    cls_type = operator.inputs[0].type.__class__
+    if cls_type != DoubleTensorType:
+        cls_type = FloatTensorType
     N = operator.inputs[0].type.shape[0]
     K = (operator.raw_operator.n_components
          if operator.type == 'SklearnTruncatedSVD'
          else operator.raw_operator.n_components_)
 
-    operator.outputs[0].type.shape = [N, K]
+    operator.outputs[0].type = cls_type([N, K])
 
 
 register_shape_calculator('SklearnIncrementalPCA',
