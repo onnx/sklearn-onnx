795ebc8eb50fb1e922e3c7fc79d7551a27ab1a7f
Xavier Dupré <xadupre@users.noreply.github.com>
2020-09-03 19:31:12 +0200

    Add complete tutorial to the documentation (#517)
    * add full tutorial to the documentation
    
    Signed-off-by: xavier dupré <xavier.dupre@gmail.com>

---
+++ docs/tests/test_utils_benchmark.py
@@ -0,0 +1,28 @@
+"""
+@brief      test log(time=3s)
+"""
+
+import unittest
+import numpy
+from skl2onnx.tutorial import measure_time
+
+
+class TestMeasureTime(unittest.TestCase):
+
+    def test_vector_count(self):
+        def fct():
+            X = numpy.ones((1000, 5))
+            return X
+        res = measure_time(
+            "fct", context={"fct": fct}, div_by_number=False, number=100)
+        self.assertIn("average", res)
+        res = measure_time(
+            "fct", context={"fct": fct}, div_by_number=True, number=100)
+        self.assertIn("average", res)
+        res = measure_time(
+            "fct", context={"fct": fct}, div_by_number=True, number=1000)
+        self.assertIn("average", res)
+
+
+if __name__ == "__main__":
+    unittest.main()
