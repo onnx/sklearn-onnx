1a78013da2b114ddc3a98080a67d19a9ced6247b
Xavier Dupré <xadupre@users.noreply.github.com>
2020-09-03 19:31:12 +0200

    Add complete tutorial to the documentation (#517)
    * add full tutorial to the documentation
    
    Signed-off-by: xavier dupré <xavier.dupre@gmail.com>

---
+++ docs/tests/test_utils_classes.py
@@ -0,0 +1,18 @@
+"""
+@brief      test log(time=3s)
+"""
+
+import unittest
+from skl2onnx.tutorial.imagenet_classes import class_names
+
+
+class TestUtilsClasses(unittest.TestCase):
+
+    def test_classes(self):
+        cl = class_names
+        self.assertIsInstance(cl, dict)
+        self.assertEqual(len(cl), 1000)
+
+
+if __name__ == "__main__":
+    unittest.main()
