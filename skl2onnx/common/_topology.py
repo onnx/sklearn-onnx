5a8e931d177ae965f23bb02c6b5d3eb97f3044eb
xavier dupré <xavier.dupre@gmail.com>
2020-10-13 18:34:49 +0200

    Update _topology.py
    Signed-off-by: xavier dupré <xavier.dupre@gmail.com>

--- skl2onnx/common/_topology.py
+++ skl2onnx/common/_topology.py
@@ -15,8 +15,13 @@ from onnxconverter_common.data_types import (  # noqa
     Int64TensorType, SequenceType,  # noqa
     StringTensorType, DoubleTensorType,
     Int32TensorType, BooleanTensorType,
-    DoubleTensorType,
-    Int8TensorType, UInt8TensorType)
+    DoubleTensorType)
+try:
+    from onnxconverter_common.data_types import (
+        Int8TensorType, UInt8TensorType)
+except ImportError:
+    Int8TensorType = None
+    UInt8TensorType = None
 from ..proto import (
     get_opset_number_from_onnx,
     get_latest_tested_opset_version
@@ -130,9 +135,11 @@ class Variable:
                 ty = Int64TensorType(shape)
             elif elem == onnx_proto.TensorProto.INT32:
                 ty = Int32TensorType(shape)
-            elif elem == onnx_proto.TensorProto.UINT8:
+            elif (UInt8TensorType is not None and
+                    elem == onnx_proto.TensorProto.UINT8):
                 ty = UInt8TensorType(shape)
-            elif elem == onnx_proto.TensorProto.INT8:
+            elif (Int8TensorType is not None and
+                    elem == onnx_proto.TensorProto.INT8):
                 ty = Int8TensorType(shape)
             elif elem == 0:
                 ty = FloatTensorType(shape)
