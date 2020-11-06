504ea94825909172222aff95bc42cbd2e3c67cb8
Xavier Dupré <xadupre@users.noreply.github.com>
2020-11-06 10:54:11 +0100

    Implements option zipmap='columns' to split output of a classifier into multiple vectors (#550)
    * Implements option zipmap='columns' to split output of a classifier into multiple vectors
    * Update test_sklearn_glm_classifier_converter.py
    * Fix issue with reserve_name, returns the key and not the value

--- skl2onnx/operator_converters/zip_map.py
+++ skl2onnx/operator_converters/zip_map.py
@@ -3,13 +3,13 @@
 # Licensed under the MIT License. See License.txt in the project root for
 # license information.
 # --------------------------------------------------------------------------
-
 from ..proto import onnx_proto
-from ..common._apply_operation import apply_cast, apply_identity
+from ..common._apply_operation import (
+    apply_slice, apply_cast, apply_identity, apply_reshape)
 from ..common._registration import register_converter
 
 
-def convert_sklearn_zipmap(scope, operator, container):
+def _common_convert_sklearn_zipmap(scope, operator, container):
     zipmap_attrs = {'name': scope.get_unique_operator_name('ZipMap')}
     to_type = onnx_proto.TensorProto.INT64
 
@@ -25,9 +25,29 @@ def convert_sklearn_zipmap(scope, operator, container):
     else:
         apply_cast(scope, operator.inputs[0].full_name,
                    operator.outputs[0].full_name, container, to=to_type)
+    return zipmap_attrs
+
+
+def convert_sklearn_zipmap(scope, operator, container):
+    zipmap_attrs = _common_convert_sklearn_zipmap(scope, operator, container)
     container.add_node('ZipMap', operator.inputs[1].full_name,
                        operator.outputs[1].full_name,
                        op_domain='ai.onnx.ml', **zipmap_attrs)
 
 
+def convert_sklearn_zipmap_columns(scope, operator, container):
+    _common_convert_sklearn_zipmap(scope, operator, container)
+    probs = operator.inputs[1].full_name
+    for i in range(1, len(operator.outputs)):
+        out = operator.outputs[i].full_name
+        flat = scope.get_unique_variable_name(out)
+        apply_slice(
+            scope, probs, flat, container, starts=[i-1], ends=[i], axes=[1],
+            operator_name=scope.get_unique_operator_name('Slice'))
+        apply_reshape(
+            scope, flat, out, container, desired_shape=(-1, ),
+            operator_name=scope.get_unique_operator_name('reshape'))
+
+
 register_converter('SklearnZipMap', convert_sklearn_zipmap)
+register_converter('SklearnZipMapColumns', convert_sklearn_zipmap_columns)
