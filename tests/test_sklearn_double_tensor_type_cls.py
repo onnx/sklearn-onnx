f69344a771cb41f77f4a174f3c804ab80cd08e1d
xavier dupré <xavier.dupre@gmail.com>
2020-12-03 16:32:05 +0100

    add svc, knn, sgd, voting, ovr
    Signed-off-by: xavier dupré <xavier.dupre@gmail.com>

--- tests/test_sklearn_double_tensor_type_cls.py
+++ tests/test_sklearn_double_tensor_type_cls.py
@@ -3,12 +3,15 @@
 import unittest
 from distutils.version import StrictVersion
 import numpy as np
+from sklearn.exceptions import ConvergenceWarning
 # from sklearn.ensemble import BaggingClassifier
 # Requires PR #488.
 # from sklearn.gaussian_process import GaussianProcessClassifier
-from sklearn.linear_model import LogisticRegression  # , SGDClassifier
-# from sklearn.neighbors import KNeighborsClassifier
-# from sklearn.neural_network import MLPClassifier
+from sklearn.linear_model import LogisticRegression, SGDClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.neural_network import MLPClassifier
+from sklearn.multiclass import OneVsRestClassifier
+from sklearn.svm import SVC
 try:
     from sklearn.ensemble import VotingClassifier
 except ImportError:
@@ -27,145 +30,192 @@ from test_utils import (
     dump_data_and_model, fit_classification_model)  # , TARGET_OPSET)
 
 TARGET_OPSET = 12  # change when PR 551
+warnings_to_skip = (DeprecationWarning, FutureWarning, ConvergenceWarning)
 
 
 class TestSklearnDoubleTensorTypeClassifier(unittest.TestCase):
+
+    def _common_classifier(
+            self, model_cls_set, name_root=None, debug=False, raw_scores=True):
+        for model_cls in model_cls_set:
+            if name_root is None:
+                name = model_cls.__name__
+            else:
+                name = name_root
+            for n_cl in [2, 3]:
+                model, X = fit_classification_model(
+                    model_cls(), n_cl, n_features=4)
+                pmethod = ('decision_function_binary' if n_cl == 2 else
+                           'decision_function')
+                bs = [True, False] if raw_scores else [False]
+                for b in bs:
+                    for z in [False]:
+                        # zipmap does not allow tensor(double) as inputs
+                        with self.subTest(n_classes=n_cl, raw_scores=b,
+                                          model=name):
+                            if raw_scores:
+                                options = {"raw_scores": b,
+                                           "zipmap": z}
+                            else:
+                                options = {"zipmap": z}
+                            model_onnx = convert_sklearn(
+                                model, "model",
+                                [("input", DoubleTensorType(
+                                    [None, X.shape[1]]))],
+                                target_opset=TARGET_OPSET,
+                                options={id(model): options})
+                            if debug:
+                                print(model_onnx)
+                            self.assertIn("elem_type: 11", str(model_onnx))
+                            methods = None if not b else ['predict', pmethod]
+                            if not b and n_cl == 2:
+                                # onnxruntime does not support sigmoid for
+                                # DoubleTensorType
+                                continue
+                            dump_data_and_model(
+                                X.astype(np.float64), model, model_onnx,
+                                methods=methods,
+                                basename="Sklearn{}Double2RAW{}"
+                                         "ZIP{}CL{}".format(
+                                            name,
+                                            1 if b else 0,
+                                            1 if z else 0, n_cl))
+
     @unittest.skipIf(
         StrictVersion(ort_version) < StrictVersion("1.5.0"),
         reason="ArgMax is missing")
-    @ignore_warnings(category=DeprecationWarning)
-    def test_model_logistic_regression_64_binary(self):
-        for n_cl in [2, 3]:
-            model, X = fit_classification_model(
-                LogisticRegression(), n_cl, n_features=4)
-            pmethod = ('decision_function_binary' if n_cl == 2 else
-                       'decision_function')
-            for b in [True, False]:
-                for z in [False]:
-                    # zipmap does not allow tensor(double) as inputs
-                    with self.subTest(n_classes=n_cl, raw_score=b):
-                        model_onnx = convert_sklearn(
-                            model, "model",
-                            [("input", DoubleTensorType([None, X.shape[1]]))],
-                            target_opset=TARGET_OPSET,
-                            options={id(model): {"raw_scores": b,
-                                                 "zipmap": z}})
-                        self.assertIn("elem_type: 11", str(model_onnx))
-                        methods = None if not b else ['predict', pmethod]
-                        if not b and n_cl == 2:
-                            # onnxruntime does not support sigmoid for
-                            # DoubleTensorType
-                            continue
-                        dump_data_and_model(
-                            X.astype(np.float64), model, model_onnx,
-                            methods=methods,
-                            basename="SklearnLogisticRegressionDouble2RAW{}"
-                                     "ZIP{}CL{}".format(
-                                        1 if b else 0, 1 if z else 0, n_cl))
+    @ignore_warnings(category=warnings_to_skip)
+    def test_model_logistic_64(self):
+        self._common_classifier([LogisticRegression])
 
-    """
     @unittest.skipIf(
-        StrictVersion(ort_version) < StrictVersion("1.6.0"),
-        reason="onnxruntime misses implementation for "
-               "Relu, Tanh, Sigmoid for double")
-    def test_model_mlpregressor_64(self):
-        # Could not find an implementation for the node Relu:Relu(6)
-        # Could not find an implementation for the node Tanh:Tanh(6)
-        # Could not find an implementation for the node Sigmoid:Sigmoid(6)
-        for activation in ['relu', 'tanh', 'logistic']:
-            with self.subTest(activation=activation):
-                model, X = fit_regression_model(
-                    MLPRegressor(activation=activation))
-                model_onnx = convert_sklearn(
-                    model, "linear regression",
-                    [("input", DoubleTensorType([None, X.shape[1]]))],
-                    target_opset=TARGET_OPSET)
-                self.assertIn("elem_type: 11", str(model_onnx))
-                dump_data_and_model(
-                    X.astype(np.float64), model, model_onnx,
-                    basename="SklearnMLPRegressorDouble%s" % activation)
+        StrictVersion(ort_version) < StrictVersion("1.5.0"),
+        reason="ArgMax is missing")
+    @ignore_warnings(category=warnings_to_skip)
+    def test_modelsgd_64(self):
+        self._common_classifier([SGDClassifier])
+        self._common_classifier([lambda: SGDClassifier(loss='hinge')],
+                                "SGDClassifierHinge")
+        self._common_classifier([lambda: SGDClassifier(loss='perceptron')],
+                                "SGDClassifierPerceptron")
 
     @unittest.skipIf(
         StrictVersion(ort_version) < StrictVersion("1.6.0"),
-        reason="onnxruntime misses implementation for "
-               "ReduceMean for double")
-    def test_bagging_regressor_sgd_64(self):
-        # Could not find an implementation for
-        # the node ReduceMean:ReduceMean(11)
-        model, X = fit_regression_model(
-            BaggingRegressor(SGDRegressor()))
-        model_onnx = convert_sklearn(
-            model, "bagging regressor",
-            [("input", DoubleTensorType([None, X.shape[1]]))],
-            target_opset=TARGET_OPSET)
-        dump_data_and_model(
-            X.astype(np.float64), model, model_onnx,
-            basename="SklearnBaggingRegressorSGDDouble")
-
-    @unittest.skipIf(
-        StrictVersion(ort_version) <= StrictVersion("1.2.0"),
-        reason="onnxruntime misses implementation for double")
-    def test_model_sgd_regressor_64(self):
-        model, X = fit_regression_model(SGDRegressor())
-        model_onnx = convert_sklearn(
-            model, "linear regression",
-            [("input", DoubleTensorType([None, X.shape[1]]))],
-            target_opset=TARGET_OPSET)
-        self.assertIn("elem_type: 11", str(model_onnx))
-        dump_data_and_model(
-            X.astype(np.float64), model, model_onnx,
-            basename="SklearnLinearSGDRegressorDouble")
+        reason="ArgMax, Reciprocal are missing")
+    @ignore_warnings(category=warnings_to_skip)
+    def test_modelsgdlog_64(self):
+        self._common_classifier([lambda: SGDClassifier(loss='log')],
+                                "SGDClassifierLog")
 
     @unittest.skipIf(
         StrictVersion(ort_version) < StrictVersion("1.6.0"),
-        reason="shape_inference fails")
-    def test_gpr_rbf_fitted_true_double(self):
-        gp = GaussianProcessRegressor(
-            alpha=1e-7, n_restarts_optimizer=15, normalize_y=True)
-        gp, X = fit_regression_model(gp)
-        model_onnx = to_onnx(
-            gp, initial_types=[('X', DoubleTensorType([None, None]))],
-            target_opset=TARGET_OPSET)
-        dump_data_and_model(
-            X.astype(np.float64), gp, model_onnx, verbose=False,
-            basename="SklearnGaussianProcessRBFTDouble")
+        reason="ArgMax, Relu are missing")
+    @ignore_warnings(category=warnings_to_skip)
+    def test_mlpclassifier_relu_64(self):
+        self._common_classifier(
+            [lambda: MLPClassifier(activation='Relu')],
+            "MLPClassifierRelu", raw_scores=False)
 
     @unittest.skipIf(
         StrictVersion(ort_version) < StrictVersion("1.6.0"),
-        reason="onnxruntime misses implementation for "
-               "TopK for double")
-    def test_model_knn_regressor_double(self):
-        # Could not find an implementation for the node To_TopK:TopK(11)
-        model, X = fit_regression_model(KNeighborsRegressor(n_neighbors=2))
-        model_onnx = convert_sklearn(
-            model, "KNN regressor", [("input", DoubleTensorType([None, 4]))],
-            target_opset=TARGET_OPSET,
-            options={id(model): {'optim': 'cdist'}})
-        dump_data_and_model(
-            X.astype(np.float64)[:7],
-            model, model_onnx,
-            basename="SklearnKNeighborsRegressorDouble")
+        reason="ArgMax, Tanh are missing")
+    @ignore_warnings(category=warnings_to_skip)
+    def test_mlpclassifier_tanh_64(self):
+        self._common_classifier(
+            [lambda: MLPClassifier(activation='tanh',
+                                   hidden_layer_sizes=(2,))],
+            "MLPClassifierTanh", raw_scores=False)
 
-    @unittest.skipIf(VotingRegressor is None, reason="new in 0.21")
     @unittest.skipIf(
         StrictVersion(ort_version) < StrictVersion("1.6.0"),
-        reason="onnxruntime misses implementation for "
-               "Sum for double")
-    def test_model_voting_regression(self):
-        # Could not find an implementation for the node Sum:Sum(8)
-        model = VotingRegressor([
-            ('lr', LinearRegression()),
-            ('dt', SGDRegressor())])
-        model, X = fit_regression_model(model)
-        model_onnx = convert_sklearn(
-            model, "voting regression",
-            [("input", DoubleTensorType([None, X.shape[1]]))],
-            target_opset=TARGET_OPSET)
-        dump_data_and_model(
-            X.astype(np.float64), model, model_onnx,
-            basename="SklearnVotingRegressorDouble",
-            comparable_outputs=[0])
-    """
+        reason="ArgMax, Sigmoid are missing")
+    @ignore_warnings(category=warnings_to_skip)
+    def test_mlpclassifier_logistic_64(self):
+        self._common_classifier(
+            [lambda: MLPClassifier(activation='logistic',
+                                   hidden_layer_sizes=(2,))],
+            "MLPClassifierLogistic", raw_scores=False)
+
+    @unittest.skipIf(
+        StrictVersion(ort_version) < StrictVersion("1.5.0"),
+        reason="ArgMax is missing")
+    @ignore_warnings(category=warnings_to_skip)
+    def test_mlpclassifier_identity_64(self):
+        self._common_classifier(
+            [lambda: MLPClassifier(activation='identity',
+                                   hidden_layer_sizes=(2,))],
+            "MLPClassifierIdentity", raw_scores=False)
+
+    @unittest.skipIf(
+        StrictVersion(ort_version) < StrictVersion("1.6.0"),
+        reason="ArgMax, TopK are missing")
+    @ignore_warnings(category=warnings_to_skip)
+    def test_knn_64(self):
+        self._common_classifier(
+            [lambda: KNeighborsClassifier()],
+            "KNeighborsClassifier", raw_scores=False)
+
+    @unittest.skipIf(
+        VotingClassifier is None, reason="scikit-learn too old")
+    @unittest.skipIf(
+        StrictVersion(ort_version) < StrictVersion("1.6.0"),
+        reason="ArgMax, Sum are missing")
+    @ignore_warnings(category=warnings_to_skip)
+    def test_voting_64(self):
+        estimators = [('a', LogisticRegression()),
+                      ('b', LogisticRegression())]
+        self._common_classifier(
+            [lambda: VotingClassifier(estimators,
+                                      flatten_transform=False)],
+            "VotingClassifier", raw_scores=False)
+
+    @unittest.skipIf(
+        StrictVersion(ort_version) < StrictVersion("1.6.0"),
+        reason="ArgMax, LpNormalization are missing")
+    @ignore_warnings(category=warnings_to_skip)
+    def test_ovr_64(self):
+        self._common_classifier(
+            [lambda: OneVsRestClassifier(LogisticRegression())],
+            "VotingClassifier", raw_scores=False)
+
+    @unittest.skipIf(
+        StrictVersion(ort_version) < StrictVersion("1.6.0"),
+        reason="ArgMax, LpNormalization are missing")
+    @ignore_warnings(category=warnings_to_skip)
+    def test_svc_linear_64(self):
+        self._common_classifier(
+            [lambda: SVC(kernel='linear')], "SVCLinear",
+            raw_scores=False)
+
+    @unittest.skipIf(
+        StrictVersion(ort_version) < StrictVersion("1.6.0"),
+        reason="ArgMax, Sum are missing")
+    @ignore_warnings(category=warnings_to_skip)
+    def test_svc_poly_64(self):
+        self._common_classifier(
+            [lambda: SVC(kernel='poly')], "SVCpoly",
+            raw_scores=False)
+
+    @unittest.skipIf(
+        StrictVersion(ort_version) < StrictVersion("1.6.0"),
+        reason="ArgMax, Sum are missing")
+    @ignore_warnings(category=warnings_to_skip)
+    def test_svc_rbf_64(self):
+        self._common_classifier(
+            [lambda: SVC(kernel='rbf')], "SVCrbf",
+            raw_scores=False)
+
+    @unittest.skipIf(
+        StrictVersion(ort_version) < StrictVersion("1.6.0"),
+        reason="ArgMax, Sum are missing")
+    @ignore_warnings(category=warnings_to_skip)
+    def test_svc_sigmoid_64(self):
+        self._common_classifier(
+            [lambda: SVC(kernel='sigmoid')], "SVCsigmoid",
+            raw_scores=False)
+
+    # BernoulliNB, CategoricalNB, ComplementNB
+    # BaggingClassifier StackingClassifier
 
 
 if __name__ == "__main__":
