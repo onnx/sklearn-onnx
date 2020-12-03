94fba201704ba2dbfb8fe05eb6e40a18ef084b58
xavier dupré <xavier.dupre@gmail.com>
2020-12-03 01:43:25 +0100

    update benchmark

--- benchmarks/bench_plot_onnxruntime_linreg.py
+++ benchmarks/bench_plot_onnxruntime_linreg.py
@@ -186,7 +186,7 @@ def plot_results(df, verbose=False):
 
 
 @ignore_warnings(category=FutureWarning)
-def run_bench(repeat=1000, verbose=False):
+def run_bench(repeat=2000, verbose=False):
     n_obs = [1, 10, 100, 1000, 10000, 100000]
     methods = ['predict']
     n_features = [10, 50, 100]
