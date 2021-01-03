0ebe36d6f112532d9693cbe78864d360d3b1bdb3
xavier dupré <xavier.dupre@gmail.com>
2021-01-03 17:08:45 +0100

    add Api11 for unsqueeze, split
    Signed-off-by: xavier dupré <xavier.dupre@gmail.com>

---
+++ tests/test_opset13.py
@@ -0,0 +1,82 @@
+# -------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See License.txt in the project root for
+# license information.
+# --------------------------------------------------------------------------
+
+import unittest
+import numpy
+from numpy.testing import assert_almost_equal
+from onnxruntime import InferenceSession
+from skl2onnx.algebra.onnx_ops import (
+    OnnxReduceSumApi11,
+    OnnxSplitApi11,
+    OnnxSqueezeApi11,
+    OnnxUnsqueezeApi11)
+
+
+class TestOpset13(unittest.TestCase):
+
+    def test_reduce_sum(self):
+        X = numpy.array([[2, 1], [0, 1]], dtype=numpy.float32)
+
+        for opset in (11, 12, 13):
+            with self.subTest(opset=opset):
+                onx = OnnxReduceSumApi11(
+                    'X', output_names=['Y'], keepdims=0, op_version=opset)
+                model_def = onx.to_onnx(
+                    {'X': X.astype(numpy.float32)}, target_opset=opset)
+                got = InferenceSession(model_def.SerializeToString()).run(
+                    None, {'X': X})
+                assert_almost_equal(numpy.sum(X), got[0], decimal=6)
+                onx = OnnxReduceSumApi11(
+                    'X', output_names=['Y'], axes=[1], op_version=opset)
+                model_def = onx.to_onnx(
+                    {'X': X.astype(numpy.float32)}, target_opset=opset)
+                got = InferenceSession(model_def.SerializeToString()).run(
+                    None, {'X': X})
+                assert_almost_equal(
+                    numpy.sum(X, axis=1, keepdims=True), got[0], decimal=6)
+
+    def test_split(self):
+        x = numpy.array([1., 2., 3., 4., 5., 6.]).astype(numpy.float32)
+        y = [numpy.array([1., 2.]).astype(numpy.float32),
+             numpy.array([3., 4.]).astype(numpy.float32),
+             numpy.array([5., 6.]).astype(numpy.float32)]
+
+        onx = OnnxSplitApi11(
+            'X', axis=0, split=[2, 2, 2],
+            output_names=['Y1', 'Y2', 'Y3'], op_version=13)
+        model_def = onx.to_onnx(
+            {'X': x.astype(numpy.float32)}, target_opset=13)
+        got = InferenceSession(model_def.SerializeToString()).run(
+            None, {'X': x})
+        assert_almost_equal(y[0], got[0])
+        assert_almost_equal(y[1], got[1])
+        assert_almost_equal(y[2], got[2])
+
+    def test_squeeze(self):
+        x = numpy.random.randn(20, 1).astype(numpy.float32)
+        y = numpy.squeeze(x)
+        onx = OnnxSqueezeApi11(
+            'X', axes=[1], output_names=['Y'], op_version=13)
+        model_def = onx.to_onnx(
+            {'X': x.astype(numpy.float32)}, target_opset=13)
+        got = InferenceSession(model_def.SerializeToString()).run(
+            None, {'X': x})
+        assert_almost_equal(y, got[0])
+
+    def test_unsqueeze(self):
+        x = numpy.random.randn(1, 3, 1, 5).astype(numpy.float32)
+        y = numpy.expand_dims(x, axis=-2)
+        onx = OnnxUnsqueezeApi11(
+            'X', axes=[-2], output_names=['Y'], op_version=13)
+        model_def = onx.to_onnx(
+            {'X': x.astype(numpy.float32)}, target_opset=13)
+        got = InferenceSession(model_def.SerializeToString()).run(
+            None, {'X': x})
+        assert_almost_equal(y, got[0])
+
+
+if __name__ == "__main__":
+    unittest.main()
