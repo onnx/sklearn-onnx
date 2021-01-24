06669139ef104a7fb1f396f7a5487faa1f151159
xavier dupré <xavier.dupre@gmail.com>
2021-01-24 18:37:05 +0100

    documentation
    Signed-off-by: xavier dupré <xavier.dupre@gmail.com>

--- skl2onnx/_supported_operators.py
+++ skl2onnx/_supported_operators.py
@@ -422,6 +422,11 @@ def update_registered_converter(model, alias, shape_fct, convert_fct,
                                     convert_sklearn_random_forest_classifier,
                                     options={'zipmap': [True, False, 'columns'],
                                              'raw_scores': [True, False]})
+
+    The function does not update the parser if not specified except if
+    option `'zipmap'` is added to the list. Every classifier
+    must declare this option to let the default parser
+    autmatically handle that option.
     """ # noqa
     if (not overwrite and model in sklearn_operator_name_map
             and alias != sklearn_operator_name_map[model]):
