a4ffb6cc4808eb5b52d42c3cac07c15b218caf11
xavier dupré <xavier.dupre@gmail.com>
2021-05-11 02:24:22 +0200

    lint
    Signed-off-by: xavier dupré <xavier.dupre@gmail.com>

--- tests/test_utils/utils_backend.py
+++ tests/test_utils/utils_backend.py
@@ -28,7 +28,7 @@ class OnnxRuntimeAssertionError(AssertionError):
     def __init__(self, msg):
         new_msg = "{}\nonnx=={} onnxruntime=={}".format(
             msg, onnx.__version__, onnxruntime.__version__)
-        AssertionError.__init__(self, msg)
+        AssertionError.__init__(self, new_msg)
 
 
 class OnnxRuntimeMissingNewOnnxOperatorException(OnnxRuntimeAssertionError):
