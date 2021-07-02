ef7d22876f669bba9a6468ef34879d8fb2fcd2b6
xavier dupré <xavier.dupre@gmail.com>
2021-07-02 14:01:05 +0200

    Support conversion of StandardScaler with Double
    Signed-off-by: xavier dupré <xavier.dupre@gmail.com>

--- skl2onnx/__init__.py
+++ skl2onnx/__init__.py
@@ -3,7 +3,7 @@
 """
 Main entry point to the converter from the *scikit-learn* to *onnx*.
 """
-__version__ = "1.9.0"
+__version__ = "1.9.1.dev"
 __author__ = "Microsoft"
 __producer__ = "skl2onnx"
 __producer_version__ = __version__
--- tests/test_sklearn_normalizer_converter.py
+++ tests/test_sklearn_normalizer_converter.py
@@ -7,7 +7,8 @@ import unittest
 import numpy
 from sklearn.preprocessing import Normalizer
 from skl2onnx import convert_sklearn
-from skl2onnx.common.data_types import Int64TensorType, FloatTensorType
+from skl2onnx.common.data_types import (
+    Int64TensorType, FloatTensorType, DoubleTensorType)
 from test_utils import dump_data_and_model
 
 
@@ -15,44 +16,68 @@ class TestSklearnNormalizerConverter(unittest.TestCase):
     def test_model_normalizer(self):
         model = Normalizer(norm="l2")
         model_onnx = convert_sklearn(
-            model,
-            "scikit-learn normalizer",
-            [("input", Int64TensorType([None, 1]))],
-        )
+            model, "scikit-learn normalizer",
+            [("input", Int64TensorType([None, 1]))])
         self.assertTrue(model_onnx is not None)
         self.assertTrue(len(model_onnx.graph.node) == 1)
 
-    def test_model_normalizer_float(self):
-        model = Normalizer(norm="l2")
+    def test_model_normalizer_float_l1(self):
+        model = Normalizer(norm="l1")
         model_onnx = convert_sklearn(
-            model,
-            "scikit-learn normalizer",
-            [("input", FloatTensorType([None, 3]))],
-        )
+            model, "scikit-learn normalizer",
+            [("input", FloatTensorType([None, 3]))])
         self.assertTrue(model_onnx is not None)
         self.assertTrue(len(model_onnx.graph.node) == 1)
         dump_data_and_model(
-            numpy.array([[1, 1, 3], [3, 1, 2]], dtype=numpy.float32),
-            model,
-            model_onnx,
-            basename="SklearnNormalizerL2-SkipDim1",
-        )
+            numpy.array([[1, -1, 3], [3, 1, 2]], dtype=numpy.float32),
+            model, model_onnx,
+            basename="SklearnNormalizerL1-SkipDim1")
+
+    def test_model_normalizer_float_l2(self):
+        model = Normalizer(norm="l2")
+        model_onnx = convert_sklearn(
+            model, "scikit-learn normalizer",
+            [("input", FloatTensorType([None, 3]))])
+        self.assertTrue(model_onnx is not None)
+        self.assertTrue(len(model_onnx.graph.node) == 1)
+        dump_data_and_model(
+            numpy.array([[1, -1, 3], [3, 1, 2]], dtype=numpy.float32),
+            model, model_onnx,
+            basename="SklearnNormalizerL2-SkipDim1")
+
+    def test_model_normalizer_double_l1(self):
+        model = Normalizer(norm="l1")
+        model_onnx = convert_sklearn(
+            model, "scikit-learn normalizer",
+            [("input", DoubleTensorType([None, 3]))])
+        self.assertTrue(model_onnx is not None)
+        dump_data_and_model(
+            numpy.array([[1, -1, 3], [3, 1, 2]], dtype=numpy.float64),
+            model, model_onnx,
+            basename="SklearnNormalizerL1Double-SkipDim1")
+
+    def test_model_normalizer_double_l2(self):
+        model = Normalizer(norm="l2")
+        model_onnx = convert_sklearn(
+            model, "scikit-learn normalizer",
+            [("input", DoubleTensorType([None, 3]))])
+        self.assertTrue(model_onnx is not None)
+        dump_data_and_model(
+            numpy.array([[1, -1, 3], [3, 1, 2]], dtype=numpy.float64),
+            model, model_onnx,
+            basename="SklearnNormalizerL2Double-SkipDim1")
 
     def test_model_normalizer_float_noshape(self):
         model = Normalizer(norm="l2")
         model_onnx = convert_sklearn(
-            model,
-            "scikit-learn normalizer",
-            [("input", FloatTensorType([]))],
-        )
+            model, "scikit-learn normalizer",
+            [("input", FloatTensorType([]))])
         self.assertTrue(model_onnx is not None)
         self.assertTrue(len(model_onnx.graph.node) == 1)
         dump_data_and_model(
-            numpy.array([[1, 1, 3], [3, 1, 2]], dtype=numpy.float32),
-            model,
-            model_onnx,
-            basename="SklearnNormalizerL2-SkipDim1",
-        )
+            numpy.array([[1, -1, 3], [3, 1, 2]], dtype=numpy.float32),
+            model, model_onnx,
+            basename="SklearnNormalizerL2NoShape-SkipDim1")
 
 
 if __name__ == "__main__":
