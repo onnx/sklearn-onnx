0ddb99b2f0cd571bae5d65ca0a5d22ade108a408
Xavier Dupré <xadupre@users.noreply.github.com>
2021-07-13 11:40:38 +0200

    Support conversion of StandardScaler with Double (#680)
    Signed-off-by: xavier dupré <xavier.dupre@gmail.com>

--- skl2onnx/common/_apply_operation.py
+++ skl2onnx/common/_apply_operation.py
@@ -24,20 +24,37 @@ def apply_normalizer(scope, inputs, outputs, container,
         # Normalizer only produces floats.
         if norm == 'L1':
             norm = scope.get_unique_variable_name('norm')
+            norm_abs = scope.get_unique_variable_name('norm_abs')
+            container.add_node(
+                'Abs', input, norm_abs,
+                name=scope.get_unique_operator_name('Abs'))
 
             if container.target_opset < 13:
                 container.add_node(
-                    'ReduceSum', input, norm, axes=[1], keepdims=1,
-                    name=scope.get_unique_operator_name('NormalizerNorm'))
+                    'ReduceSum', norm_abs, norm, axes=[1], keepdims=1,
+                    name=scope.get_unique_operator_name('ReduceSum'))
             else:
                 axis_name = scope.get_unique_variable_name('axis')
                 container.add_initializer(
                     axis_name, onnx_proto.TensorProto.INT64, [1], [1])
                 container.add_node(
-                    'ReduceSum', [input, axis_name], norm, keepdims=1,
+                    'ReduceSum', [norm_abs, axis_name], norm, keepdims=1,
                     name=scope.get_unique_operator_name('ReduceSum'))
             apply_div(  # noqa
                 scope, [input, norm], output, container,
+                operator_name=scope.get_unique_operator_name(
+                    'NormalizerNorm'))
+        elif norm == 'L2':
+            norm = scope.get_unique_variable_name('norm')
+            norm2 = scope.get_unique_variable_name('norm2')
+            container.add_node(
+                'ReduceSumSquare', input, norm, axes=[1], keepdims=1,
+                name=scope.get_unique_operator_name('ReduceSumSquare'))
+            container.add_node(
+                'Sqrt', [norm], norm2,
+                name=scope.get_unique_operator_name('Sqrt'))
+            apply_div(  # noqa
+                scope, [input, norm2], output, container,
                 operator_name=scope.get_unique_operator_name(
                     'NormalizerNorm'))
         else:
