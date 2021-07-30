b64f42d57238d030df61afc208f8f8d8bd515b38
Xavier Dupré <xadupre@users.noreply.github.com>
2021-07-31 01:56:36 +0200

    Fix Scaler converter, fix OnnxSubEstimator not directly connected to the converter inputs (#692)
    * Fix bug with SubEstimator not directly connected to the inputs
    * fix scaler converter

--- skl2onnx/common/_container.py
+++ skl2onnx/common/_container.py
@@ -735,14 +735,22 @@ class ModelComponentContainer(ModelContainer, _WhiteBlackContainer):
         if len(missing_ops) > 0:
             def nstr(name):
                 if name in order:
-                    return "%s(%d)" % (name, order[name])
+                    return "%s#%d" % (name, order[name])
                 return name
-            rows = ["%s (%s) -> (%s)" % (
+            rows = ["%s(%s) -> [%s]" % (
                 n.name or n.op_type,
                 ', '.join(map(nstr, n.input)),
                 ', '.join(n.output))
                 for n in missing_ops]
             rows.insert(0, "")
+            rows.append("--")
+            rows.append("--all-nodes--")
+            rows.append("--")
+            rows.extend("%s(%s) -> [%s]" % (
+                n.name or n.op_type,
+                ', '.join(map(nstr, n.input)),
+                ', '.join(n.output))
+                for n in self.nodes)
             raise RuntimeError(
                 "After %d iterations for %d nodes, still unable "
                 "to sort names %r. The graph may be disconnected. "
