7e8c86f5966027ea63aeb6a2d7289b2eda651885
Xavier Dupré <xadupre@users.noreply.github.com>
2021-08-20 12:39:19 +0200

    Publish documentation 1.9.2 (#707)
    * update documentation

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.9.1.dev',
+    VERSION: '1.9.2',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
