0650082428f6d433d8c29e7f2071bd5d53a4f43f
xavier dupré <xavier.dupre@gmail.com>
2021-11-19 13:01:07 +0100

    change documentation theme
    Signed-off-by: xavier dupré <xavier.dupre@gmail.com>

--- docs/examples/plot_pipeline_xgboost.py
+++ docs/examples/plot_pipeline_xgboost.py
@@ -16,10 +16,6 @@ a pipeline including a *XGBoost* model. *sklearn-onnx* can convert
 the whole pipeline as long as it knows the converter associated to
 a *XGBClassifier*. Let's see how to do it.
 
-A couple of errors might happen while trying to convert
-your own pipeline, some of them are described
-and explained in :ref:`errors-pipeline`.
-
 .. contents::
     :local:
 
