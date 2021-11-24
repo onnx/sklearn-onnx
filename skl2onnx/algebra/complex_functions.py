a3517e5f9a44ee8ef4dfc7bd7765076bdfc2799a
Xavier Dupré <xadupre@users.noreply.github.com>
2021-11-24 20:24:10 +0100

    Fix converters for MinMaxScaler, GaussianProcessRegressor (Scan) (#791)
    * Issue with GaussianProcessRegressor
    * Fix converters for MinMaxScaler, converter using CDist
    * MinMaxScaler.clip introduced in 0.24

--- skl2onnx/algebra/complex_functions.py
+++ skl2onnx/algebra/complex_functions.py
@@ -1,13 +1,16 @@
 # SPDX-License-Identifier: Apache-2.0
 
+from logging import getLogger
 from collections import OrderedDict
 import numpy as np
 from ..common.data_types import FloatTensorType, DoubleTensorType
 from ..common.utils import get_unique_subgraph
 from .onnx_ops import (
     OnnxIdentity, OnnxScan, OnnxTranspose,
-    OnnxSub, OnnxReduceSumSquare, OnnxSqueezeApi11,
+    OnnxSub, OnnxReduceSumSquare,
     OnnxSqrt, OnnxPow, OnnxAbs, OnnxReduceSumApi11)
+
+logger = getLogger('skl2onnx')
 
 
 def onnx_squareform_pdist(X, metric='sqeuclidean', dtype=None,
@@ -38,17 +41,16 @@ def _onnx_squareform_pdist_sqeuclidean(X, dtype=None, op_version=None,
                    op_version=op_version)
     id_next = OnnxIdentity('next_in', output_names=['next_out'],
                            op_version=op_version)
-    norm = OnnxReduceSumSquare(diff, axes=[1], op_version=op_version)
-    flat = OnnxSqueezeApi11(norm, output_names=['scan_out'], axes=[1],
-                            op_version=op_version)
+    flat = OnnxReduceSumSquare(diff, axes=[1], op_version=op_version,
+                               output_names=['scan_out'], keepdims=0)
     flat.set_onnx_name_prefix('cflat_%d' % unique)
     id_next.set_onnx_name_prefix('pdistsqe_%d' % unique)
     tensor_type = FloatTensorType if dtype == np.float32 else DoubleTensorType
     scan_body = id_next.to_onnx(
-        OrderedDict([('next_in', tensor_type()),
-                     ('next', tensor_type())]),
-        outputs=[('next_out', tensor_type()),
-                 ('scan_out', tensor_type())],
+        OrderedDict([('next_in', tensor_type([None, None])),
+                     ('next', tensor_type([None]))]),
+        outputs=[('next_out', tensor_type([None, None])),
+                 ('scan_out', tensor_type([None]))],
         other_outputs=[flat],
         target_opset=op_version)
 
@@ -56,6 +58,8 @@ def _onnx_squareform_pdist_sqeuclidean(X, dtype=None, op_version=None,
                     num_scan_inputs=1,
                     body=(scan_body.graph, [id_next, flat]),
                     op_version=op_version, **kwargs)
+    logger.debug('[_onnx_squareform_pdist_sqeuclidean] +Scan dtype=%r',
+                 dtype)
     return node[1]
 
 
@@ -117,15 +121,17 @@ def _onnx_cdist_end(XA, XB, id_next, flat, dtype, op_version,
     tensor_type = FloatTensorType if dtype == np.float32 else DoubleTensorType
     id_next.set_onnx_name_prefix('cdistd_%d' % unique)
     flat.set_onnx_name_prefix('cdistdf_%d' % unique)
-    shape_in = (tensor_type() if dim_in is None
+    shape_in = (tensor_type([None, None]) if dim_in is None
                 else tensor_type([None, dim_in]))
     scan_body = id_next.to_onnx(
         OrderedDict([('next_in', shape_in),
-                     ('next', tensor_type())]),
-        outputs=[('next_out', tensor_type()),
-                 ('scan_out', tensor_type())],
+                     ('next', tensor_type([None]))]),
+        outputs=[('next_out', tensor_type([None, None])),
+                 ('scan_out', tensor_type([None]))],
         other_outputs=[flat],
         target_opset=op_version)
+    logger.debug('[_onnx_cdist_end] + Scan dim_in=%r dim_out=%r dtype=%r',
+                 dim_in, dim_out, dtype)
 
     node = OnnxScan(XA, XB, output_names=['u(scan0)', 'u(scan1)'],
                     num_scan_inputs=1,
