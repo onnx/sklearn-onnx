8614141f4ca0f754e0df49e4dd09102a24d73fe9
xavier dupré <xavier.dupre@gmail.com>
2022-02-22 17:51:45 +0100

    Fix SimpleImputer with strategy most_frequent
    Signed-off-by: xavier dupré <xavier.dupre@gmail.com>

--- skl2onnx/operator_converters/imputer_op.py
+++ skl2onnx/operator_converters/imputer_op.py
@@ -41,16 +41,13 @@ def convert_sklearn_imputer(scope: Scope, operator: Operator,
             # loop on features
             fill_value = scope.get_unique_variable_name("fillvalue")
             if op.fill_value is None:
-                print('-----------')
-                print(op.statistics_)
-                print(op.indicator_)
-                print(op.strategy)
-                raise ValueError(
-                    "fill_value is not specified in the model %r." % op)
+                skl_fill_value = op.statistics_[i]
+            else:
+                skl_fill_value = op.fill_value
             container.add_node(
                 "LabelEncoder", [zero], [fill_value],
                 keys_int64s=[0], values_strings=[op.statistics_[i]],
-                default_string=op.fill_value, op_domain='ai.onnx.ml',
+                default_string=skl_fill_value, op_domain='ai.onnx.ml',
                 op_version=2)
             init = scope.get_unique_variable_name("i%d" % i)
             container.add_initializer(
