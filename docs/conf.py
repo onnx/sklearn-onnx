e85674a67a0a043e19c2ffe181e5d31eca8ce40b
Xavier Dupré <xadupre@users.noreply.github.com>
2022-02-25 15:31:04 +0100

    Fix documentation configuration (#839)

--- docs/conf.py
+++ docs/conf.py
@@ -61,7 +61,7 @@ pygments_style = 'default'
 html_theme = "sphinx_mo"
 html_static_path = ['_static']
 html_theme = "pydata_sphinx_theme"
-html_theme_path = pydata_sphinx_theme.get_html_theme_path()
+html_theme_path = ['_static']
 html_logo = "logo_main.png"
 
 # -- Options for graphviz ----------------------------------------------------
