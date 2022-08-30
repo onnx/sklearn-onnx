abc705f3d27a0abfefb8aaa34ce4d665272493cc
xiaowuhu <xiaowuhu@microsoft.com>
2022-08-30 15:54:30 +0800

    Update test_sklearn_gamma_regressor.py
    Signed-off-by: xiaowuhu <xiaowuhu@microsoft.com>

--- tests/test_sklearn_gamma_regressor.py
+++ tests/test_sklearn_gamma_regressor.py
@@ -50,7 +50,7 @@ class TestGammaRegressorConverter(unittest.TestCase):
     def test_gamma_regressor_int(self):
 
         model = GammaRegressor()
-        X = np.array([[1, 2], [2, 3], [3, 4], [4, 3]])
+        X = np.array([[10, 20], [20, 30], [30, 40], [40, 30]])
         y = np.array([19, 26, 33, 30])
         model.fit(X, y)
         test_x = np.array([[1, 0], [2, 8]])
@@ -62,18 +62,19 @@ class TestGammaRegressorConverter(unittest.TestCase):
             target_opset=TARGET_OPSET)
 
         self.assertIsNotNone(model_onnx is not None)
-        dump_data_and_model(test_x.astype(np.float32), model, model_onnx,
+        dump_data_and_model(test_x.astype(np.int64), model, model_onnx,
                             basename="SklearnGammaRegressor")
 
+    
     @unittest.skipIf(GammaRegressor is None,
                      reason="scikit-learn<1.0")
     def test_gamma_regressor_double(self):
 
         model = GammaRegressor()
-        X = np.array([[1, 2], [2, 3], [3, 4], [4, 3]])
+        X = np.array([[1.1, 2.1], [2.3, 3.2], [3.2, 4.3], [4.2, 3.1]])
         y = np.array([19, 26, 33, 30])
         model.fit(X, y)
-        test_x = np.array([[1, 0], [2, 8]])
+        test_x = np.array([[1.1, 0.1], [2.2, 8.4]])
 
         model_onnx = convert_sklearn(
             model,
@@ -82,8 +83,8 @@ class TestGammaRegressorConverter(unittest.TestCase):
             target_opset=TARGET_OPSET)
 
         self.assertIsNotNone(model_onnx is not None)
-        dump_data_and_model(test_x.astype(np.float32), model, model_onnx,
+        dump_data_and_model(test_x.astype(np.double), model, model_onnx,
                             basename="SklearnGammaRegressor")
-
+        
 if __name__ == "__main__":
     unittest.main(verbosity=3)
