067004de1ab1432a006051836cfafc581923912b
xadupre <xadupre@microsoft.com>
2022-11-21 14:10:36 +0100

    disable some configurations
    Signed-off-by: xadupre <xadupre@microsoft.com>

--- tests/test_optimisation.py
+++ tests/test_optimisation.py
@@ -14,6 +14,8 @@ from test_utils import TARGET_OPSET
 
 class TestOptimisation(unittest.TestCase):
 
+    @skipif.unittest(TARGET_OPSET <= 14,
+                     reason="only verified with opset 15+")
     def test_coptimisation_identity_removal(self):
         # investigation issue #854
 
