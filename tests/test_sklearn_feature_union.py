f1774815aa8da42e6bcbc2ac4ca6a2b85bd7b0cb
Zachary Tessler <ztessler@gmail.com>
2022-12-16 16:01:56 -0500

    flake8 line length
    Signed-off-by: Zachary Tessler <ztessler@gmail.com>

--- tests/test_sklearn_feature_union.py
+++ tests/test_sklearn_feature_union.py
@@ -45,8 +45,8 @@ class TestSklearnAdaBoostModels(unittest.TestCase):
         data = load_iris()
         X, y = data.data, data.target
         X = X.astype(np.float32)
-        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5,
-                                               random_state=42)
+        X_train, X_test, y_train, y_test = train_test_split(
+                X, y, test_size=0.5, random_state=42)
         model = FeatureUnion([
             ('features', FeatureUnion([
                 ('standard', StandardScaler()),
