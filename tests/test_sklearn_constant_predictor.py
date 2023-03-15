edf1c6234d33a749395b00dd3c64a32fb1fc2d9f
xadupre <xadupre@microsoft.com>
2023-03-15 16:40:53 +0100

    add test for double
    Signed-off-by: xadupre <xadupre@microsoft.com>

--- tests/test_sklearn_constant_predictor.py
+++ tests/test_sklearn_constant_predictor.py
@@ -6,7 +6,7 @@ import unittest
 import numpy as np
 from sklearn.multiclass import _ConstantPredictor
 from onnxruntime import __version__ as ort_version
-from skl2onnx import convert_sklearn
+from skl2onnx import to_onnx
 
 from skl2onnx.common.data_types import FloatTensorType, DoubleTensorType
 
@@ -26,15 +26,32 @@ class TestConstantPredictorConverter(unittest.TestCase):
         model.fit(X, y)
         test_x = np.array([[1, 0], [2, 8]])
 
-        model_onnx = convert_sklearn(
+        model_onnx = to_onnx(
             model, "scikit-learn ConstantPredictor",
-            [("input", FloatTensorType([None, X.shape[1]]))],
+            initial_types=[("input", FloatTensorType([None, X.shape[1]]))],
             target_opset=TARGET_OPSET,
             options={'zipmap': False})
 
         self.assertIsNotNone(model_onnx is not None)
         dump_data_and_model(test_x.astype(np.float32), model, model_onnx,
-                            basename="SklearnConstantPredictor")
+                            basename="SklearnConstantPredictorFloat")
+
+    def test_constant_predictor_double(self):
+        model = _ConstantPredictor()
+        X = np.array([[1, 2]])
+        y = np.array([0])
+        model.fit(X, y)
+        test_x = np.array([[1, 0], [2, 8]])
+
+        model_onnx = to_onnx(
+            model, "scikit-learn ConstantPredictor",
+            initial_types=[("input", DoubleTensorType([None, X.shape[1]]))],
+            target_opset=TARGET_OPSET,
+            options={'zipmap': False})
+
+        self.assertIsNotNone(model_onnx is not None)
+        dump_data_and_model(test_x.astype(np.float64), model, model_onnx,
+                            basename="SklearnConstantPredictorDouble")
 
 
 if __name__ == "__main__":
