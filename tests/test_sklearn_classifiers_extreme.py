3f16c4abee3a555d7b7e2dd50c4ec56fac6cc6ec
Xavier Dupre <xadupre@microsoft.com>
2023-08-01 09:50:58 +0200

    black
    Signed-off-by: Xavier Dupre <xadupre@microsoft.com>

--- tests/test_sklearn_classifiers_extreme.py
+++ tests/test_sklearn_classifiers_extreme.py
@@ -10,7 +10,6 @@ from test_utils import TARGET_OPSET
 
 
 class TestSklearnClassifiersExtreme(unittest.TestCase):
-
     def test_one_training_class(self):
         x = np.eye(4, dtype=np.float32)
         y = np.array([5, 5, 5, 5], dtype=np.int64)
@@ -19,13 +18,14 @@ class TestSklearnClassifiersExtreme(unittest.TestCase):
         cl = cl.fit(x, y)
 
         expected = [cl.predict(x), cl.predict_proba(x)]
-        onx = to_onnx(cl, x, target_opset=TARGET_OPSET,
-                      options={"zipmap": False})
+        onx = to_onnx(cl, x, target_opset=TARGET_OPSET, options={"zipmap": False})
 
-        for cls in [lambda onx: ReferenceEvaluator(onx, verbose=0),
-                    lambda onx: InferenceSession(
-                        onx.SerializeToString(),
-                        providers=["CPUExecutionProvider"])]:
+        for cls in [
+            lambda onx: ReferenceEvaluator(onx, verbose=0),
+            lambda onx: InferenceSession(
+                onx.SerializeToString(), providers=["CPUExecutionProvider"]
+            ),
+        ]:
             sess = cls(onx)
             res = sess.run(None, {"X": x})
             self.assertEqual(len(res), len(expected))
