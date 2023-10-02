f6d42a51c480a9aa69f2e407a21a1eac10a24a90
Xavier Dupre <xadupre@microsoft.com>
2023-10-02 12:46:17 +0200

    Checks if categories indices to drop are not None for OneHotEncoder
    Signed-off-by: Xavier Dupre <xadupre@microsoft.com>

--- tests/test_sklearn_one_hot_encoder_converter.py
+++ tests/test_sklearn_one_hot_encoder_converter.py
@@ -455,12 +455,44 @@ class TestSklearnOneHotEncoderConverter(unittest.TestCase):
             self.assertEqual(expected.shape, res[0].shape)
             assert_almost_equal(expected, res[0])
 
+    @unittest.skipIf(
+        not one_hot_encoder_supports_drop(),
+        reason="OneHotEncoder does not support drop in scikit versions < 0.21",
+    )
+    def test_one_hot_encoder_drop_if_binary(self):
+        data = [
+            ["c0.4", "c0.2", 0],
+            ["c1.4", "c1.2", 0],
+            ["c0.2", "c2.2", 1],
+            ["c0.2", "c2.2", 1],
+            ["c0.2", "c2.2", 1],
+            ["c0.2", "c2.2", 1],
+        ]
+        test = [["c0.2", "c2.2", 1]]
+        model = OneHotEncoder(categories="auto", drop="if_binary")
+        model.fit(data)
+        inputs = [
+            ("input1", StringTensorType([None, 2])),
+            ("input2", Int64TensorType([None, 1])),
+        ]
+        model_onnx = convert_sklearn(
+            model, "one-hot encoder", inputs, target_opset=TARGET_OPSET
+        )
+        self.assertTrue(model_onnx is not None)
+        dump_data_and_model(
+            test,
+            model,
+            model_onnx,
+            verbose=False,
+            basename="SklearnOneHotEncoderMixedStringIntDrop",
+        )
+
 
 if __name__ == "__main__":
     import logging
 
-    for name in ["skl2onnx", "onnx-extended"]:
+    for name in ["skl2onnx"]:
         log = logging.getLogger(name)
         log.setLevel(logging.ERROR)
-    TestSklearnOneHotEncoderConverter().test_model_one_hot_encoder()
+    TestSklearnOneHotEncoderConverter().test_one_hot_encoder_drop_if_binary()
     unittest.main(verbosity=2)
