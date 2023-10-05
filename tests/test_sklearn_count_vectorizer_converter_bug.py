1972d8d9e7a5cc9968bf4dab6fe8d1e85bfb63e4
Xavier Dupre <xadupre@microsoft.com>
2023-10-05 15:02:48 +0200

    investigate
    Signed-off-by: Xavier Dupre <xadupre@microsoft.com>

--- tests/test_sklearn_count_vectorizer_converter_bug.py
+++ tests/test_sklearn_count_vectorizer_converter_bug.py
@@ -5,10 +5,20 @@ Tests scikit-learn's count vectorizer converter.
 """
 import unittest
 import numpy
+from numpy.testing import assert_almost_equal
 from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
+from sklearn.linear_model import LogisticRegression
+from sklearn.pipeline import Pipeline
+
+try:
+    from onnx.reference import ReferenceEvaluator
+except ImportError:
+    ReferenceEvaluator = None
+from onnxruntime import InferenceSession
 from skl2onnx import convert_sklearn
 from skl2onnx.common.data_types import StringTensorType
 from test_utils import dump_data_and_model, TARGET_OPSET
+from test_utils.reference_implementation_text import Tokenizer
 
 
 class TestSklearnCountVectorizerBug(unittest.TestCase):
@@ -76,6 +86,89 @@ class TestSklearnCountVectorizerBug(unittest.TestCase):
             basename="SklearnTfidfVectorizer12Wngram-OneOff-SklCol",
         )
 
+    @unittest.skipIf(TARGET_OPSET < 10, reason="not available")
+    def test_model_count_vectorizer_short_length(self):
+        corpus = [
+            "This is the first document.",
+            "This document is the second document.",
+            "And this is the third one.",
+            "Is this the first document?",
+        ]
+
+        labels = ["a", "b", "c", "d"]
+
+        to_inference = [
+            ".",
+            "first",
+            ". .",
+            "document one",
+            "first and",
+            "document",
+            "and",
+        ]
+
+        vectorizer = CountVectorizer(
+            max_features=5,
+            analyzer="word",
+            ngram_range=(1, 1),
+            encoding="utf8",
+            strip_accents=None,
+            token_pattern=(
+                r"\b[a-zA-Z0-9_]+\b"
+                r"|"
+                r"[\~\-!.,:;@+&<>*={}\[\]№?()^|/%$#'`\"\\_]"
+                r"|"
+                r"\d+"
+            ),
+        )
+        vectorizer.fit_transform(corpus)
+        classifier = LogisticRegression()
+
+        model = Pipeline(steps=[("vectorizer", vectorizer), ("classifier", classifier)])
+        model.fit(corpus, labels)
+        expected_probas = model.predict_proba(to_inference)
+        expected_labels = model.predict(to_inference)
+
+        onnx_model = convert_sklearn(
+            model,
+            initial_types=[("X", StringTensorType([None, None]))],
+            options={"zipmap": False},
+        )
+
+        session = InferenceSession(
+            onnx_model.SerializeToString(), providers=["CPUExecutionProvider"]
+        )
+
+        ref = (
+            None
+            if ReferenceEvaluator is None
+            else ReferenceEvaluator(onnx_model, new_ops=[Tokenizer])
+        )
+
+        for x in to_inference:
+            feeds = {"X": numpy.array([[x]])}
+            expected = model.predict([x])
+            got = session.run(None, feeds)
+            self.assertEqual(expected.tolist(), got[0].tolist())
+            if ref is None:
+                continue
+            got = ref.run(None, feeds)
+            self.assertEqual(expected.tolist(), got[0].tolist())
+
+        feeds = {"X": numpy.array([to_inference]).T}
+        got = session.run(None, feeds)
+        self.assertEqual(expected_labels.tolist(), got[0].tolist())
+        assert_almost_equal(expected_probas, got[1])
+        if ref is not None:
+            got = ref.run(None, feeds)
+            self.assertEqual(expected_labels.tolist(), got[0].tolist())
+            assert_almost_equal(expected_probas, got[1])
+
 
 if __name__ == "__main__":
-    unittest.main()
+    import logging
+
+    logger = logging.getLogger("skl2onnx")
+    logger.setLevel(logging.ERROR)
+    TestSklearnCountVectorizerBug().test_model_count_vectorizer_short_length()
+    unittest.main(verbosity=2)
