ebd20e91fc400f0bf84fb6ba43557968316c3e3a
Xavier Dupré <xadupre@users.noreply.github.com>
2024-06-19 10:44:44 +0200

    Add converter for TunedThresholdClassifierCV (#1107)
    * Add converter for TunedThresholdClassifierCV
    
    Signed-off-by: Xavier Dupre <xadupre@microsoft.com>
    
    * upgrade version
    
    Signed-off-by: Xavier Dupre <xadupre@microsoft.com>
    
    * documentation
    
    Signed-off-by: Xavier Dupre <xadupre@microsoft.com>
    
    * update numpy version
    
    Signed-off-by: Xavier Dupre <xadupre@microsoft.com>
    
    * do not use numpy 2
    
    Signed-off-by: Xavier Dupre <xadupre@microsoft.com>
    
    * delay import
    
    Signed-off-by: Xavier Dupre <xadupre@microsoft.com>
    
    ---------
    
    Signed-off-by: Xavier Dupre <xadupre@microsoft.com>

---
+++ skl2onnx/shape_calculators/tuned_threshold_classifier.py
@@ -0,0 +1,16 @@
+# SPDX-License-Identifier: Apache-2.0
+
+from ..common._registration import register_shape_calculator
+from ..common.utils import check_input_and_output_numbers
+from ..common.shape_calculator import _infer_linear_classifier_output_types
+
+
+def tuned_threshold_classifier_shape_calculator(operator):
+    check_input_and_output_numbers(operator, output_count_range=2)
+
+    _infer_linear_classifier_output_types(operator)
+
+
+register_shape_calculator(
+    "SklearnTunedThresholdClassifierCV", tuned_threshold_classifier_shape_calculator
+)
